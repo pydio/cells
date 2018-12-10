5212930702bcd37b57c72201c963da9858a88300
Greg Hecquet <greg.tf.hecquet@gmail.com>
2018-12-10 11:44:38 +0100

    One broker to rule them all

---
+++ common/micro/broker/broker.go
@@ -0,0 +1,59 @@
+package broker
+
+import "github.com/micro/go-micro/broker"
+
+type brokerwrap struct {
+	b    broker.Broker
+	opts Options
+}
+
+// NewBroker wraps a standard broker but prevents it from disconnecting while there still is a service running
+func NewBroker(b broker.Broker, opts ...Option) broker.Broker {
+	return &brokerwrap{b, newOptions(opts...)}
+}
+
+// Options wraps standard function
+func (b *brokerwrap) Options() broker.Options {
+	return b.b.Options()
+}
+
+// Address wraps standard function
+func (b *brokerwrap) Address() string {
+	return b.b.Address()
+}
+
+// Connect wraps standard function
+func (b *brokerwrap) Connect() error {
+	return b.b.Connect()
+}
+
+// Disconnect handles the disconnection to the broker. It prevents it if there is a service that is still active
+func (b *brokerwrap) Disconnect() error {
+	for _, o := range b.opts.beforeDisconnect {
+		if err := o(); err != nil {
+			return err
+		}
+	}
+
+	return b.b.Disconnect()
+}
+
+// Init wraps standard function
+func (b *brokerwrap) Init(opts ...broker.Option) error {
+	return b.b.Init(opts...)
+}
+
+// Publish wraps standard function
+func (b *brokerwrap) Publish(s string, m *broker.Message, opts ...broker.PublishOption) error {
+	return b.b.Publish(s, m, opts...)
+}
+
+// Publish wraps standard function
+func (b *brokerwrap) Subscribe(s string, h broker.Handler, opts ...broker.SubscribeOption) (broker.Subscriber, error) {
+	return b.b.Subscribe(s, h, opts...)
+}
+
+// Publish wraps standard function
+func (b *brokerwrap) String() string {
+	return b.b.String()
+}
--- common/micro/broker/nats/nats.go
+++ common/micro/broker/nats/nats.go
@@ -1,27 +1,46 @@
 package nats
 
 import (
-	"github.com/micro/go-micro/broker"
+	"fmt"
+
+	microbroker "github.com/micro/go-micro/broker"
 	"github.com/micro/go-micro/client"
 	"github.com/micro/go-micro/server"
 	"github.com/micro/go-plugins/broker/nats"
 	"github.com/pydio/cells/common/micro"
+	"github.com/pydio/cells/common/micro/broker"
+	"github.com/pydio/cells/common/registry"
 	"github.com/spf13/viper"
 )
 
+// Enable the nats broker
 func Enable() {
-
 	addr := viper.GetString("broker_address")
+	b := broker.NewBroker(
+		nats.NewBroker(microbroker.Addrs(addr)),
+		broker.BeforeDisconnect(func() error {
+			s, err := registry.ListRunningServices()
+			if err != nil {
+				return err
+			}
+			if len(s) > 0 {
+				return fmt.Errorf("services are still running")
+			}
+
+			return nil
+		}),
+	)
+
 	defaults.InitServer(func() server.Option {
-		return server.Broker(nats.NewBroker(broker.Addrs(addr)))
+		return server.Broker(b)
 	})
 
 	defaults.InitClient(func() client.Option {
-		return client.Broker(nats.NewBroker(broker.Addrs(addr)))
+		return client.Broker(b)
 	})
 
-	broker.DefaultBroker = nats.NewBroker(broker.Addrs(addr))
+	microbroker.DefaultBroker = b
 
 	// Establishing connectin
-	broker.Connect()
+	microbroker.Connect()
 }
