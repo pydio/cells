dfda8b6f05e0268a724073f347bbc551420cf40d
Greg Hecquet <greg.tf.hecquet@gmail.com>
2019-03-11 17:44:44 +0100

    Removing log

--- data/source/sync/lib/endpoints/client_s3.go
+++ data/source/sync/lib/endpoints/client_s3.go
@@ -537,7 +537,6 @@ func (c *S3Client) Watch(recursivePath string) (*common.WatchObject, error) {
 				}
 				objectPath = c.getLocalPath(objectPath)
 
-				fmt.Println("HERE WE ARE ", record)
 				if strings.HasPrefix(record.EventName, "s3:ObjectCreated:") {
 					log.Logger(c.globalContext).Debug("S3 Event", zap.String("event", "ObjectCreated"), zap.Any("event", record))
 					eventChan <- common.EventInfo{
