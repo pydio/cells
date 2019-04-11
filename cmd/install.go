ee38907c44a2134ec68b5e85275985d240d49441
Bruno Sinou <bruno.sinou@posteo.de>
2019-04-11 19:12:40 +0200

    Enable let'S Encrypt + document supported configurations

--- cmd/install.go
+++ cmd/install.go
@@ -361,10 +361,10 @@ func init() {
 
 	flags := installCmd.PersistentFlags()
 	flags.StringVar(&niBindUrl, "bind", "", "[Non interactive mode] internal URL:PORT on which the main proxy will bind. Self-signed SSL will be used by default")
-	flags.StringVar(&niExtUrl, "external", "", "[Non interactive mode] external URL:PORT exposed to outside")
-	flags.BoolVar(&niDisableSsl, "no_ssl", false, "[Non interactive mode] do not enable https")
-	flags.StringVar(&niCertFile, "ssl_cert_file", "", "[Non interactive mode] ssl cert file path, left empty for self signed automatically")
-	flags.StringVar(&niKeyFile, "ssl_key_file", "", "[Non interactive mode] ssl key file path, left empty for self signed automatically")
+	flags.StringVar(&niExtUrl, "external", "", "[Non interactive mode] external PROTOCOL:URL:PORT exposed to the outside")
+	flags.BoolVar(&niDisableSsl, "no_ssl", false, "[Non interactive mode] use raw http (no TLS)")
+	flags.StringVar(&niCertFile, "ssl_cert_file", "", "[Non interactive mode] ssl cert file path")
+	flags.StringVar(&niKeyFile, "ssl_key_file", "", "[Non interactive mode] ssl key file path")
 	flags.StringVar(&niLeEmailContact, "le_email", "", "[Non interactive mode] contact e-mail for Let's Encrypt provided certificate")
 	flags.BoolVar(&niLeAcceptEula, "le_agree", false, "[Non interactive mode] accept Let's Encrypt EULA")
 
