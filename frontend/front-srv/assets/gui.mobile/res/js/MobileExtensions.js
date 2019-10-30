e103b921366345bbaea1a86c54588efa79228cba
cdujeu <charles@ajaxplorer.info>
2019-10-30 15:00:19 +0100

    Update smart-banner app ids

--- frontend/front-srv/assets/gui.mobile/res/js/MobileExtensions.js
+++ frontend/front-srv/assets/gui.mobile/res/js/MobileExtensions.js
@@ -69,7 +69,7 @@ class MobileExtensions extends Component{
             daysHidden: 15,   // days to hide banner after close button is clicked (defaults to 15)
             daysReminder: 90, // days to hide banner after "VIEW" button is clicked (defaults to 90)
             appStoreLanguage: 'us', // language code for the App Store (defaults to user's browser language)
-            title: 'Pydio Pro',
+            title: 'Pydio',
             author: 'Abstrium SAS',
             button: 'VIEW',
             store: {
@@ -77,9 +77,9 @@ class MobileExtensions extends Component{
                 android: 'In Google Play'
             },
             price: {
-                ios: '0,99€',
-                android: '0,99€'
-            }
+                ios: 'FREE',
+                android: 'FREE'
+            },
             //, theme: '' // put platform type ('ios', 'android', etc.) here to force single theme on all device
             // , icon: '' // full path to icon image if not using website icon image
             //, force: 'android' // Uncomment for platform emulation
