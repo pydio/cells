e710543c5aa9fad6dd72e166445ebc4e078daf30
cdujeu <charles@ajaxplorer.info>
2020-02-10 18:26:00 +0100

    Update conf i18n libraries

--- frontend/front-srv/rest/plugins.go
+++ frontend/front-srv/rest/plugins.go
@@ -46,7 +46,6 @@ var BasePluginsBox = frontend.PluginBox{
 		"action.user",
 		"auth.pydio",
 		"authfront.session_login",
-		"conf.pydio",
 		"core.activitystreams",
 		"core.auth",
 		"core.authfront",
