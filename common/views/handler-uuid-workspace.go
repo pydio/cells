26ea6031097ed92ac7a6bedec44fd02a6b77ab43
cdujeu <charles@ajaxplorer.info>
2020-03-25 09:00:54 +0100

    Fix AppearsIn in UuidRouter (multi workspaces, workspaces with multiple roots)

--- common/views/handler-uuid-workspace.go
+++ common/views/handler-uuid-workspace.go
@@ -22,6 +22,7 @@ package views
 
 import (
 	"context"
+	"path"
 	"strings"
 
 	"github.com/micro/go-micro/errors"
@@ -113,8 +114,8 @@ func (h *UuidNodeHandler) updateOutputBranch(ctx context.Context, node *tree.Nod
 		workspaces, wsRoots := accessList.BelongsToWorkspaces(ctx, ancestors...)
 		log.Logger(ctx).Debug("Belongs to workspaces", zap.Any("ws", workspaces), zap.Any("wsRoots", wsRoots))
 		for _, ws := range workspaces {
-			if relativePath, e := h.relativePathToWsRoot(ctx, node.Path, wsRoots[ws.UUID]); e == nil {
-				out.AppearsIn = append(node.AppearsIn, &tree.WorkspaceRelativePath{
+			if relativePath, e := h.relativePathToWsRoot(ctx, ws, node.Path, wsRoots[ws.UUID]); e == nil {
+				out.AppearsIn = append(out.AppearsIn, &tree.WorkspaceRelativePath{
 					WsUuid:  ws.UUID,
 					WsLabel: ws.Label,
 					WsSlug:  ws.Slug,
@@ -131,12 +132,18 @@ func (h *UuidNodeHandler) updateOutputBranch(ctx context.Context, node *tree.Nod
 
 }
 
-func (h *UuidNodeHandler) relativePathToWsRoot(ctx context.Context, nodeFullPath string, rootNodeId string) (string, error) {
+func (h *UuidNodeHandler) relativePathToWsRoot(ctx context.Context, ws *idm.Workspace, nodeFullPath string, rootNodeId string) (string, error) {
 
 	if resp, e := h.next.ReadNode(ctx, &tree.ReadNodeRequest{Node: &tree.Node{Uuid: rootNodeId}}); e == nil {
 		rootPath := resp.Node.Path
 		if strings.HasPrefix(nodeFullPath, rootPath) {
-			return strings.TrimPrefix(nodeFullPath, rootPath), nil
+			relPath := strings.TrimPrefix(nodeFullPath, rootPath)
+			if len(ws.RootUUIDs) > 1 {
+				// This workspace has multiple root, prepend the fake root key
+				rootKey := h.makeRootKey(resp.Node)
+				relPath = path.Join(rootKey, relPath)
+			}
+			return relPath, nil
 		} else {
 			return "", errors.NotFound("RouterUuid", "Cannot subtract paths "+nodeFullPath+" - "+rootPath)
 		}
