d9c797a0ade66b0c10bcadf770dfb227d5aff3bc
cdujeu <charles@ajaxplorer.info>
2020-10-15 10:58:04 +0200

    Try to fix maintenance packr box

--- common/caddy/maintenance/assets.go
+++ common/caddy/maintenance/assets.go
@@ -3,6 +3,6 @@ package maintenance
 import "github.com/pydio/packr"
 
 var (
-	// PydioInstallBox holds the root of the pydio install static files
-	PydioMaintenanceBox = packr.NewBox("./src")
+	// PydioInstallBox holds the root of the pydio maintenance static page
+	PydioMaintenanceBox = packr.NewBox("../maintenance/src")
 )
