63b5a066106ca3166e6335a11b5eb36b32403b22
cdujeu <charles@ajaxplorer.info>
2020-11-10 12:50:29 +0100

    fix wrong import

--- vendor/github.com/pydio/minio-go/api-notification.go
+++ vendor/github.com/pydio/minio-go/api-notification.go
@@ -25,7 +25,7 @@ import (
 	"net/url"
 	"time"
 
-	"github.com/minio/minio-go/pkg/s3utils"
+	"github.com/pydio/minio-go/pkg/s3utils"
 )
 
 // GetBucketNotification - get bucket notification at a given path.
