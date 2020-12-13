966f72c0c58eae562b32d02d793a74843a8e0370
cdujeu <charles@ajaxplorer.info>
2020-12-13 19:28:39 +0100

    Util tool to trigger assets reload (for dev only)

---
+++ cmd/doc-reload-assets.go
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2018. Abstrium SAS <team (at) pydio.com>
+ * This file is part of Pydio Cells.
+ *
+ * Pydio Cells is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pydio Cells is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Pydio Cells.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <https://pydio.com>.
+ */
+
+package cmd
+
+import (
+	"github.com/micro/go-micro/broker"
+	"github.com/spf13/cobra"
+
+	"github.com/pydio/cells/common"
+)
+
+// docDepsCmd shows dependencies between services.
+var reloadAssetsCmd = &cobra.Command{
+	Use:   "reload-assets",
+	Short: "Trigger frontend assets reload",
+	Long:  `Used for development only, will clear in-memory assets and refresh all, including i18n json files.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Println("Sending a reload command on ReloadAssets topic")
+		broker.Publish(common.TopicReloadAssets, &broker.Message{Body: []byte("reload")})
+	},
+}
+
+func init() {
+
+	DocCmd.AddCommand(reloadAssetsCmd)
+}
