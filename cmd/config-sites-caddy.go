14f468f5a85c089968352366c29307c89997be2d
cdujeu <charles@ajaxplorer.info>
2020-12-21 12:09:20 +0100

    config sites caddy : try to wait before returning command

--- cmd/config-sites-caddy.go
+++ cmd/config-sites-caddy.go
@@ -1,6 +1,11 @@
 package cmd
 
 import (
+	"fmt"
+	"time"
+
+	"github.com/manifoldco/promptui"
+
 	"github.com/micro/go-micro/broker"
 	"github.com/pydio/cells/common"
 	"github.com/spf13/cobra"
@@ -15,11 +20,14 @@ var sitesCaddy = &cobra.Command{
 	Short: "Dump content of the caddy file currently served by pydio.gateway.proxy",
 	Long:  "This command sends an event recognized by pydio.gateway.proxy to make it dump its caddy file in the logs.",
 	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Sending a DebugPrintInternals event to service gateway.proxy...")
 		broker.Publish(common.TopicServiceRegistration, &broker.Message{
 			Header: map[string]string{
 				common.EventHeaderServiceRegisterService: common.ServiceGatewayProxy,
 			},
 			Body: []byte(common.EventTypeDebugPrintInternals),
 		})
+		<-time.After(1 * time.Second)
+		fmt.Println(promptui.IconGood + " Done!")
 	},
 }
