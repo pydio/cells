ba556d4ee7a8af549e836d38bb34a204fcaea0df
cdujeu <charles@ajaxplorer.info>
2021-08-03 14:43:29 +0200

    Switch memory broker to a pub/sub implementation

--- common/micro/broker/memory/memory.go
+++ common/micro/broker/memory/memory.go
@@ -9,33 +9,28 @@ import (
 	"sync"
 	"time"
 
-	"github.com/google/uuid"
+	"github.com/cskr/pubsub"
 	"github.com/micro/go-micro/broker"
 	"github.com/micro/misc/lib/addr"
 )
 
 var (
-	MAX_GOROUTINES = 1000000
+	PubSubBufferCapacity = 10000
 )
 
 type memoryBroker struct {
-	opts broker.Options
-
-	addr string
 	sync.RWMutex
-	connected   bool
-	count       int64
-	events      chan struct{}
-	Subscribers map[string][]*memorySubscriber
+
+	opts      broker.Options
+	addr      string
+	connected bool
+	bus       *pubsub.PubSub
 }
 
 type memorySubscriber struct {
-	id    string
 	topic string
-
-	exit    chan bool
-	handler broker.Handler
-	opts    broker.SubscribeOptions
+	exit  chan bool
+	opts  broker.SubscribeOptions
 }
 
 func (m *memoryBroker) Options() broker.Options {
@@ -53,17 +48,17 @@ func (m *memoryBroker) Connect() error {
 	if m.connected {
 		return nil
 	}
+	m.bus = pubsub.New(PubSubBufferCapacity)
 
 	// use 127.0.0.1 to avoid scan of all network interfaces
-	addr, err := addr.Extract("127.0.0.1")
+	add, err := addr.Extract("127.0.0.1")
 	if err != nil {
 		return err
 	}
 	i := rand.Intn(20000)
-	// set addr with port
-	addr = net.JoinHostPort(addr, fmt.Sprintf("%d", 10000+i))
-
-	m.addr = addr
+	// set add with port
+	add = net.JoinHostPort(add, fmt.Sprintf("%d", 10000+i))
+	m.addr = add
 	m.connected = true
 
 	return nil
@@ -78,6 +73,7 @@ func (m *memoryBroker) Disconnect() error {
 	}
 
 	m.connected = false
+	m.bus.Shutdown()
 
 	return nil
 }
@@ -95,39 +91,9 @@ func (m *memoryBroker) Publish(topic string, msg *broker.Message, opts ...broker
 		m.RUnlock()
 		return errors.New("not connected")
 	}
-
-	subs, ok := m.Subscribers[topic]
 	m.RUnlock()
-	if !ok {
-		return nil
-	}
 
-	for _, sub := range subs {
-		m.events <- struct{}{}
-
-		done := make(chan struct{}, 1)
-		go func(sub2 *memorySubscriber, done chan struct{}) {
-			defer close(done)
-			if err := sub2.handler(&memoryEvent{topic: topic, message: msg}); err != nil {
-				fmt.Println("Broker publication to subscriber error ", topic, err)
-				//if eh := sub.opts.ErrorHandler; eh != nil {
-				//	eh(msg, err)
-				//}
-				return
-			}
-		}(sub, done)
-
-		select {
-		case <-time.After(500 * time.Millisecond):
-			fmt.Println("Broker publication to subscriber timed out ", topic)
-			break
-		case <-done:
-			break
-		}
-
-		<-m.events
-	}
-
+	m.bus.Pub(msg, topic)
 	return nil
 }
 
@@ -144,30 +110,23 @@ func (m *memoryBroker) Subscribe(topic string, handler broker.Handler, opts ...b
 		o(&options)
 	}
 
+	listener := m.bus.Sub(topic)
 	sub := &memorySubscriber{
-		exit:    make(chan bool, 1),
-		id:      uuid.New().String(),
-		topic:   topic,
-		handler: handler,
-		opts:    options,
+		exit:  make(chan bool, 1),
+		topic: topic,
+		opts:  options,
 	}
 
-	m.Lock()
-	m.Subscribers[topic] = append(m.Subscribers[topic], sub)
-	m.Unlock()
-
 	go func() {
-		<-sub.exit
-		m.Lock()
-		var newSubscribers []*memorySubscriber
-		for _, sb := range m.Subscribers[topic] {
-			if sb.id == sub.id {
-				continue
+		defer m.bus.Unsub(listener, topic)
+		for {
+			select {
+			case msg := <-listener:
+				handler(&memoryEvent{topic: topic, message: msg.(*broker.Message)})
+			case <-sub.exit:
+				return
 			}
-			newSubscribers = append(newSubscribers, sb)
 		}
-		m.Subscribers[topic] = newSubscribers
-		m.Unlock()
 	}()
 
 	return sub, nil
@@ -219,8 +178,6 @@ func NewBroker(opts ...broker.Option) broker.Broker {
 	}
 
 	return &memoryBroker{
-		opts:        options,
-		events:      make(chan struct{}, MAX_GOROUTINES),
-		Subscribers: make(map[string][]*memorySubscriber),
+		opts: options,
 	}
 }
