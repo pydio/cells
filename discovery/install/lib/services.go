8a708ef18fcb677a3ab111e31b59588d99e723ff
charles <charles@pydio.com>
2022-02-15 15:31:59 +0100

    Re-adapt registry scheme

--- discovery/install/lib/services.go
+++ discovery/install/lib/services.go
@@ -84,7 +84,7 @@ var (
 func ListServicesWithStorage() (ss []service.Service, e error) {
 	loadRegistry.Do(func() {
 		ctx := context.Background()
-		reg, err := registry.OpenRegistry(ctx, "memory:///?cache=shared")
+		reg, err := registry.OpenRegistry(ctx, "mem:///?cache=shared")
 		if err != nil {
 			e = err
 		}
