f012a1a41745a1c90d0a097320f73f2ce2e3a094
charles <charles@pydio.com>
2022-03-10 13:28:57 +0100

    Gprc mocks: wrap and capture Healthcheck request

--- common/client/grpc/grpc.go
+++ common/client/grpc/grpc.go
@@ -3,6 +3,7 @@ package grpc
 import (
 	"context"
 	"fmt"
+	"google.golang.org/grpc/health/grpc_health_v1"
 	"os"
 	"runtime/debug"
 	"strconv"
@@ -195,5 +196,21 @@ func (cc *clientConn) NewStream(ctx context.Context, desc *grpc.StreamDesc, meth
 
 // RegisterMock registers a stubbed ClientConnInterface for a given service
 func RegisterMock(serviceName string, mock grpc.ClientConnInterface) {
-	mox[serviceName] = mock
+	mox[serviceName] = &mockHSWrapper{mock: mock}
+}
+
+type mockHSWrapper struct {
+	mock grpc.ClientConnInterface
+}
+
+func (m *mockHSWrapper) Invoke(ctx context.Context, method string, args interface{}, reply interface{}, opts ...grpc.CallOption) error {
+	if method == "/grpc.health.v1.Health/Check" {
+		reply = grpc_health_v1.HealthCheckResponse{Status: grpc_health_v1.HealthCheckResponse_SERVING}
+		return nil
+	}
+	return m.mock.Invoke(ctx, method, args, reply, opts...)
+}
+
+func (m *mockHSWrapper) NewStream(ctx context.Context, desc *grpc.StreamDesc, method string, opts ...grpc.CallOption) (grpc.ClientStream, error) {
+	return m.mock.NewStream(ctx, desc, method, opts...)
 }
