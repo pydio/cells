fe36f6b37445011e04e25276eac1fc6433283a53
charles <charles@pydio.com>
2022-09-06 14:19:24 +0200

    Auto-DL of minio executable : add a lock file to avoid processes collision, verify sha1 for integrity, pass an ENV to change target location, display Release information.

--- data/source/objects/grpc/handler.go
+++ data/source/objects/grpc/handler.go
@@ -23,8 +23,9 @@ package grpc
 import (
 	"bufio"
 	"context"
+	"crypto/sha1"
+	"encoding/hex"
 	"fmt"
-	minio "github.com/minio/minio/cmd"
 	"go.uber.org/zap"
 	"io"
 	"io/ioutil"
@@ -34,7 +35,9 @@ import (
 	"path/filepath"
 	"runtime"
 	"strings"
+	"time"
 
+	minio "github.com/minio/minio/cmd"
 	_ "github.com/minio/minio/cmd/gateway"
 	"github.com/pkg/errors"
 
@@ -58,23 +61,56 @@ func (o *ObjectHandler) Name() string {
 
 func (o *ObjectHandler) LookupPath() string {
 	ext := ""
+	dir := ""
 	if runtime.GOOS == "windows" {
 		ext = ".exe"
 	}
-	crtExe, er := os.Executable()
-	if er != nil {
-		return ""
+	if d := os.Getenv("MINIO_EXECUTABLE_DIR"); d != "" {
+		if s, e := os.Stat(d); e == nil && s.IsDir() {
+			dir = d
+		}
 	}
-	return filepath.Join(filepath.Dir(crtExe), "minio"+ext)
+	if dir == "" {
+		crtExe, er := os.Executable()
+		if er != nil {
+			return ""
+		}
+		dir = filepath.Dir(crtExe)
+	}
+	return filepath.Join(dir, "minio"+ext)
 }
 
 func (o *ObjectHandler) Downloader(ctx context.Context, target string) error {
+	tmpDL := filepath.Join(filepath.Dir(target), "minio.dl.tmp")
+	lock := filepath.Join(filepath.Dir(target), "minio.lock")
+	try := 0
+	for {
+		if _, e := os.Stat(lock); os.IsNotExist(e) || try > 10 {
+			break
+		}
+		try++
+		log.Logger(ctx).Info("Minio executable already downloading, presumably by another process. Wait 30s and retry...")
+		<-time.After(20 * time.Second)
+	}
+	if try > 0 {
+		// There was a retry, check if target was created by another process in-between
+		if _, e := os.Stat(target); e == nil {
+			log.Logger(ctx).Info("Found Minio executable, do not trigger download")
+			return nil
+		}
+	}
+	// Create lock file
+	if lf, e := os.OpenFile(lock, os.O_CREATE|os.O_WRONLY, 0755); e == nil {
+		defer func() {
+			_ = lf.Close()
+			_ = os.Remove(lock)
+		}()
+	}
 	ext := ""
 	if runtime.GOOS == "windows" {
 		ext = ".exe"
 	}
 	latest := fmt.Sprintf("https://dl.min.io/server/minio/release/%s-%s/minio%s", runtime.GOOS, runtime.GOARCH, ext)
-	// latestCheck := latest + ".sha256sum"
 	log.Logger(ctx).Info("Starting download of " + latest + ", please wait...")
 	resp, e := http.Get(latest)
 	if e != nil {
@@ -85,17 +121,46 @@ func (o *ObjectHandler) Downloader(ctx context.Context, target string) error {
 		content, _ := ioutil.ReadAll(resp.Body)
 		return fmt.Errorf("wrong status code: %d, error was %s", resp.StatusCode, string(content))
 	}
-	f, e := os.OpenFile(target, os.O_CREATE|os.O_WRONLY, 0777)
+	f, e := os.OpenFile(tmpDL, os.O_CREATE|os.O_WRONLY, 0777)
 	if e != nil {
 		return e
 	}
-	defer f.Close()
-	n, e := io.Copy(f, resp.Body)
+	h := sha1.New()
+	n, e := io.Copy(f, io.TeeReader(resp.Body, h))
 	if e != nil {
+		_ = f.Close()
 		return fmt.Errorf("could not read request body: %s", e.Error())
 	}
-	log.Logger(ctx).Info(fmt.Sprintf("Downloaded %s (size %d) to %s", latest, n, target))
-	return nil
+	_ = f.Close()
+	sum := hex.EncodeToString(h.Sum(nil))
+
+	// Load SHASUM file and compare with computed sum
+	latestSumURL := latest + ".shasum"
+	r, e := http.Get(latestSumURL)
+	if e != nil {
+		return fmt.Errorf("cannot load sum file %v", e)
+	}
+	if r.StatusCode != 200 {
+		return fmt.Errorf("cannot load sum file %d:%s", r.StatusCode, r.Status)
+	}
+
+	defer r.Body.Close()
+	c, er := io.ReadAll(r.Body)
+	if er != nil {
+		return fmt.Errorf("cannot parse sum file contents: %v", er)
+	}
+	ss := strings.Split(strings.TrimSpace(string(c)), " ")
+	if len(ss) < 2 {
+		return fmt.Errorf("cannot parse sum file contents")
+	}
+	expectedSum, releaseInfo := ss[0], ss[1]
+	if expectedSum != sum {
+		return fmt.Errorf("could not properly verify download integrity, expected %s, got %s", expectedSum, sum)
+	} else {
+		log.Logger(ctx).Info(fmt.Sprintf("Downloaded and verified %s (size %d) to %s - (version %s)", latest, n, target, releaseInfo))
+	}
+
+	return os.Rename(tmpDL, target)
 
 }
 
@@ -223,7 +288,7 @@ func (o *ObjectHandler) StartMinioServer(ctx context.Context, minioServiceName s
 		minioExe = exe
 	} else if path := o.LookupPath(); path != "" {
 		if _, e := os.Stat(path); e == nil {
-			log.Logger(ctx).Info("Found minio executable next to cells executable")
+			log.Logger(ctx).Info("Found minio executable at " + path)
 			minioExe = path
 		} else if er := o.Downloader(ctx, path); er == nil {
 			minioExe = path
