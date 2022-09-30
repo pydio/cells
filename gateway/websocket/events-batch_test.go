86601e5f2cf7cd10290d9c518e4204ecd5b1b542
charles <charles@pydio.com>
2022-09-30 10:22:13 +0200

    Re-adpat events-batch_test

--- gateway/websocket/events-batch_test.go
+++ gateway/websocket/events-batch_test.go
@@ -1,6 +1,7 @@
 package websocket
 
 import (
+	"context"
 	"testing"
 	"time"
 
@@ -17,11 +18,15 @@ func TestEventBatcher(t *testing.T) {
 		dispatcher := make(chan *NodeChangeEventWithInfo)
 		done := make(chan string)
 		var dispatched []*NodeChangeEventWithInfo
+		var dispatchedCtx context.Context
+
 		clean := make(chan struct{})
+		originalCtx := context.WithValue(context.Background(), "originalKey", "originalValue")
 		go func() {
 			for {
 				select {
 				case e := <-dispatcher:
+					dispatchedCtx = e.ctx
 					dispatched = append(dispatched, e)
 				case finished := <-done:
 					delete(batchers, finished)
@@ -35,16 +40,24 @@ func TestEventBatcher(t *testing.T) {
 		nodeUUID := "test-uuid"
 		batcher := NewEventsBatcher(1*time.Second, nodeUUID, dispatcher, done)
 		batchers[nodeUUID] = batcher
-		batcher.in <- &tree.NodeChangeEvent{
-			Type:   tree.NodeChangeEvent_CREATE,
-			Target: &tree.Node{Uuid: nodeUUID},
+		batcher.in <- &NodeChangeEventWithInfo{
+			NodeChangeEvent: tree.NodeChangeEvent{
+				Type:   tree.NodeChangeEvent_CREATE,
+				Target: &tree.Node{Uuid: nodeUUID},
+			},
+			ctx: originalCtx,
 		}
-		batcher.in <- &tree.NodeChangeEvent{
-			Type:   tree.NodeChangeEvent_UPDATE_CONTENT,
-			Target: &tree.Node{Uuid: nodeUUID},
+		batcher.in <- &NodeChangeEventWithInfo{
+			NodeChangeEvent: tree.NodeChangeEvent{
+				Type:   tree.NodeChangeEvent_UPDATE_CONTENT,
+				Target: &tree.Node{Uuid: nodeUUID},
+			},
+			ctx: originalCtx,
 		}
 		<-time.After(1500 * time.Millisecond)
 
+		So(dispatchedCtx, ShouldNotBeNil)
+		So(dispatchedCtx.Value("originalKey"), ShouldEqual, "originalValue")
 		So(dispatched, ShouldHaveLength, 1)
 		So(batchers, ShouldHaveLength, 0)
 		So(batcher.closed, ShouldBeTrue)
