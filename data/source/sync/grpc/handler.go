3119a933d586f2bbd4e8da9e5a0eb31535e7cd52
charles <charles@pydio.com>
2022-11-02 14:26:27 +0100

    Revert context passed to sync.Run as it is passed along to SessionData and then it is closed when used. To be improved.

--- data/source/sync/grpc/handler.go
+++ data/source/sync/grpc/handler.go
@@ -576,9 +576,11 @@ func (s *Handler) TriggerResync(c context.Context, req *protosync.ResyncRequest)
 		}
 	}
 
-	// Copy context
-	//bg := context.Background()
-	bg := metadata.WithUserNameMetadata(c, common.PydioSystemUsername)
+	// Create a context from background
+	// TODO : extend request context (c) instead, to allow sync.Run cancellation. This requires changes on SetSessionData()
+	// as it will otherwise pass along this (c) to the patch processor, which is async, and which receives a cancelled ctx.
+	bg := context.Background()
+	bg = metadata.WithUserNameMetadata(bg, common.PydioSystemUsername)
 	bg = servicecontext.WithServiceName(bg, servicecontext.GetServiceName(c))
 	if s, o := servicecontext.SpanFromContext(c); o {
 		bg = servicecontext.WithSpan(bg, s)
