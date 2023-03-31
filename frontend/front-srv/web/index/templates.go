15383a8d38d459f1ab97b1aea41af83b001f4fa8
charles <charles@pydio.com>
2023-03-31 12:24:08 +0200

    Double load of CSS in minisites

--- frontend/front-srv/web/index/templates.go
+++ frontend/front-srv/web/index/templates.go
@@ -116,7 +116,6 @@ var Public = `<!DOCTYPE html>
 		{{if .Rebase}}<base href="{{.Rebase}}"/>{{end}}
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-		<link rel="stylesheet" type="text/css" href="{{.ResourcesFolder}}/dist/pydio.{{.Theme}}.min.css?v={{.Version}}">
 		<script language="javascript" type="text/javascript" src="{{.ResourcesFolder}}/dist/boot/pydio.boot.min.js?v={{.Version}}"></script>
 		<link rel="icon" type="image/x-png" href="{{.Favicon}}">
 	</head>
