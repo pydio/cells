7c323e7a9db998f4d8212359f89a4d6877e4d514
charles <charles@pydio.com>
2023-05-16 09:21:33 +0200

    Regenerate IDM protos after cherry-picking StreamPolicies

--- common/proto/idm/cells-idm.validator.pb.go
+++ common/proto/idm/cells-idm.validator.pb.go
@@ -7,9 +7,9 @@ import (
 	fmt "fmt"
 	math "math"
 	proto "google.golang.org/protobuf/proto"
-	_ "github.com/pydio/cells/v4/common/proto/tree"
 	_ "github.com/mwitkow/go-proto-validators"
 	_ "github.com/pydio/cells/v4/common/proto/service"
+	_ "github.com/pydio/cells/v4/common/proto/tree"
 	github_com_mwitkow_go_proto_validators "github.com/mwitkow/go-proto-validators"
 )
 
