5c1ad49ad70aeeb8138ec7f2309fba98b34db5ca
charles <charles@pydio.com>
2023-05-24 16:53:33 +0200

    Fix public link when search is enabled - missing copyrights in index package.

--- frontend/front-srv/web/index/public.go
+++ frontend/front-srv/web/index/public.go
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2023. Abstrium SAS <team (at) pydio.com>
+ * This file is part of Pydio Cells.
+ *
+ * Pydio Cells is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pydio Cells is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Pydio Cells.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <https://pydio.com>.
+ */
+
 package index
 
 import (
@@ -45,7 +65,7 @@ func (h *PublicHandler) computeTplConf(req *http.Request, linkId string) (status
 		ResourcesFolder:  "plug/gui.ajax/res",
 		Favicon:          "plug/gui.ajax/res/themes/common/images/favicon.png",
 		Theme:            "material",
-		Version:          common.Version().String(),
+		Version:          frontend.VersionHash(),
 	}
 	if customHeader := config.Get("frontend", "plugin", "gui.ajax", "HTML_CUSTOM_HEADER").String(); customHeader != "" {
 		tplConf.CustomHTMLHeader = template.HTML(customHeader)
