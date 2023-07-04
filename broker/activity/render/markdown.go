666f3352d3a99d75204d359e479755617ead93d4
charles <charles@pydio.com>
2023-07-04 15:59:18 +0200

    Activities Digest : use displayName, add date, deduplicate activities appearing in multiple workspaces/cells

--- broker/activity/render/markdown.go
+++ broker/activity/render/markdown.go
@@ -40,36 +40,42 @@ const (
 )
 
 type ServerLinks struct {
-	URLS     map[ServerUrlType]*url.URL
-	UrlFuncs map[ServerUrlType]func(object *activity.Object) string
+	URLS            map[ServerUrlType]*url.URL
+	UrlFuncs        map[ServerUrlType]func(object *activity.Object, label string) string
+	DateInterpreter func(object *activity.Object, timestamp int64) string
 }
 
 func NewServerLinks() *ServerLinks {
 	s := &ServerLinks{}
 	s.URLS = make(map[ServerUrlType]*url.URL)
-	s.UrlFuncs = make(map[ServerUrlType]func(object *activity.Object) string)
+	s.UrlFuncs = make(map[ServerUrlType]func(object *activity.Object, label string) string)
 	return s
 }
 
-func (s *ServerLinks) objectURL(urlType ServerUrlType, object *activity.Object) string {
+func (s *ServerLinks) objectURL(urlType ServerUrlType, object *activity.Object, linkLabel string) string {
+	linkLabel = html.EscapeString(linkLabel)
 	if fu, o := s.UrlFuncs[urlType]; o {
-		return fu(object)
+		return fu(object, linkLabel)
 	} else if sUrl, ok := s.URLS[urlType]; ok {
 		objectId := url.PathEscape(object.Id)
+		var ur string
 		if sUrl.Host == "" {
-			return sUrl.Scheme + "://" + strings.TrimLeft(objectId, "/")
+			ur = sUrl.Scheme + "://" + strings.TrimLeft(objectId, "/")
 		} else {
-			return sUrl.String() + strings.TrimLeft(objectId, "/")
+			ur = sUrl.String() + strings.TrimLeft(objectId, "/")
 		}
+		return MakeMarkdownLink(ur, linkLabel)
 	} else {
-		return ""
+		return linkLabel
 	}
 }
 
-func makeMarkdownLink(url string, label string) string {
+// MakeMarkdownLink creates a simple [%s](%s) link
+func MakeMarkdownLink(url string, label string) string {
 	return fmt.Sprintf("[%s](%s)", label, url)
 }
 
+// Markdown recursively renders a hierarchy of activity.Object
 func Markdown(object *activity.Object, pointOfView activity.SummaryPointOfView, language string, links ...*ServerLinks) string {
 
 	T := lang.T(language)
@@ -86,6 +92,12 @@ func Markdown(object *activity.Object, pointOfView activity.SummaryPointOfView, 
 	if object.Object != nil {
 		templateData["Object"] = Markdown(object.Object, pointOfView, language, links...)
 	}
+	var datePart string
+	if pointOfView == activity.SummaryPointOfView_GENERIC && object.Updated != nil && len(links) > 0 {
+		if interpreter := links[0].DateInterpreter; interpreter != nil {
+			datePart = interpreter(object, object.Updated.Seconds)
+		}
+	}
 
 	switch object.Type {
 	case activity.ObjectType_Digest:
@@ -99,12 +111,7 @@ func Markdown(object *activity.Object, pointOfView activity.SummaryPointOfView, 
 	case activity.ObjectType_Workspace, activity.ObjectType_Cell:
 
 		var workspaceString string
-		var wsIdentifier string
-		if link := sLinks.objectURL(ServerUrlTypeWorkspaces, object); link != "" {
-			wsIdentifier = makeMarkdownLink(link, object.Name)
-		} else {
-			wsIdentifier = object.Name
-		}
+		wsIdentifier := sLinks.objectURL(ServerUrlTypeWorkspaces, object, object.Name)
 		wsLang := T("Workspace")
 		if object.Type == activity.ObjectType_Cell {
 			wsLang = T("Cell")
@@ -126,7 +133,7 @@ func Markdown(object *activity.Object, pointOfView activity.SummaryPointOfView, 
 		} else if pointOfView == activity.SummaryPointOfView_SUBJECT {
 			return T("CreatedBy", templateData)
 		} else {
-			return T("CreatedObjectBy", templateData)
+			return T("CreatedObjectBy", templateData) + datePart
 		}
 
 	case activity.ObjectType_Move:
@@ -136,7 +143,7 @@ func Markdown(object *activity.Object, pointOfView activity.SummaryPointOfView, 
 		} else if pointOfView == activity.SummaryPointOfView_SUBJECT {
 			return T("MovedBy", templateData)
 		} else {
-			return T("MovedObjectBy", templateData)
+			return T("MovedObjectBy", templateData) + datePart
 		}
 
 	case activity.ObjectType_Delete:
@@ -146,7 +153,7 @@ func Markdown(object *activity.Object, pointOfView activity.SummaryPointOfView, 
 		} else if pointOfView == activity.SummaryPointOfView_SUBJECT {
 			return T("DeletedBy", templateData)
 		} else {
-			return T("DeletedObjectBy", templateData)
+			return T("DeletedObjectBy", templateData) + datePart
 		}
 
 	case activity.ObjectType_Update:
@@ -156,7 +163,7 @@ func Markdown(object *activity.Object, pointOfView activity.SummaryPointOfView, 
 		} else if pointOfView == activity.SummaryPointOfView_SUBJECT {
 			return T("ModifiedBy", templateData)
 		} else {
-			return T("ModifiedObjectBy", templateData)
+			return T("ModifiedObjectBy", templateData) + datePart
 		}
 
 	case activity.ObjectType_UpdateMeta:
@@ -166,7 +173,7 @@ func Markdown(object *activity.Object, pointOfView activity.SummaryPointOfView, 
 		} else if pointOfView == activity.SummaryPointOfView_SUBJECT {
 			return T("MetaModifiedBy", templateData)
 		} else {
-			return T("MetaModifiedObjectBy", templateData)
+			return T("MetaModifiedObjectBy", templateData) + datePart
 		}
 
 	case activity.ObjectType_UpdateComment:
@@ -176,7 +183,7 @@ func Markdown(object *activity.Object, pointOfView activity.SummaryPointOfView, 
 		} else if pointOfView == activity.SummaryPointOfView_SUBJECT {
 			return T("CommentedBy", templateData)
 		} else {
-			return T("CommentedObjectBy", templateData)
+			return T("CommentedObjectBy", templateData) + datePart
 		}
 
 	case activity.ObjectType_Read:
@@ -186,7 +193,7 @@ func Markdown(object *activity.Object, pointOfView activity.SummaryPointOfView, 
 		} else if pointOfView == activity.SummaryPointOfView_SUBJECT {
 			return T("AccessedBy", templateData)
 		} else {
-			return T("AccessedObjectBy", templateData)
+			return T("AccessedObjectBy", templateData) + datePart
 		}
 
 	case activity.ObjectType_Share:
@@ -195,18 +202,12 @@ func Markdown(object *activity.Object, pointOfView activity.SummaryPointOfView, 
 		} else if pointOfView == activity.SummaryPointOfView_SUBJECT {
 			return T("SharedWs", templateData)
 		} else {
-			return T("SharedWsWithYou", templateData)
+			return T("SharedWsWithYou", templateData) + datePart
 		}
 
 	case activity.ObjectType_Folder:
 
-		var docIdentifier string
-		sBase := html.EscapeString(path.Base(object.Name))
-		if link := sLinks.objectURL(ServerUrlTypeDocs, object); link != "" {
-			docIdentifier = makeMarkdownLink(link, sBase)
-		} else {
-			docIdentifier = sBase
-		}
+		docIdentifier := sLinks.objectURL(ServerUrlTypeDocs, object, path.Base(object.Name))
 		prefix := T("Folder") + " "
 		if pointOfView == activity.SummaryPointOfView_ACTOR {
 			prefix = strings.ToLower(prefix)
@@ -215,13 +216,7 @@ func Markdown(object *activity.Object, pointOfView activity.SummaryPointOfView, 
 
 	case activity.ObjectType_Document:
 
-		var docIdentifier string
-		sBase := html.EscapeString(path.Base(object.Name))
-		if link := sLinks.objectURL(ServerUrlTypeDocs, object); link != "" {
-			docIdentifier = makeMarkdownLink(link, sBase)
-		} else {
-			docIdentifier = sBase
-		}
+		docIdentifier := sLinks.objectURL(ServerUrlTypeDocs, object, path.Base(object.Name))
 		prefix := T("Document") + " "
 		if pointOfView == activity.SummaryPointOfView_ACTOR {
 			prefix = strings.ToLower(prefix)
@@ -230,14 +225,7 @@ func Markdown(object *activity.Object, pointOfView activity.SummaryPointOfView, 
 
 	case activity.ObjectType_Person:
 
-		var userIdentifier string
-		uName := html.EscapeString(object.Name)
-		if link := sLinks.objectURL(ServerUrlTypeUsers, object); link != "" {
-			userIdentifier = makeMarkdownLink(link, uName)
-		} else {
-			userIdentifier = path.Base(uName)
-		}
-		return userIdentifier
+		return sLinks.objectURL(ServerUrlTypeUsers, object, object.Name)
 
 	case activity.ObjectType_Event:
 
