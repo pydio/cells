fd1db9bd92f979d95e0c598d28883d3df6792009
charles <charles@pydio.com>
2023-08-08 14:50:06 +0200

    Goque: prefer unique Queue to PrefixQueue for performances

--- common/utils/queue/goque/goque.go
+++ common/utils/queue/goque/goque.go
@@ -25,7 +25,8 @@ func init() {
 }
 
 type serviceQueue struct {
-	q  *goque.PrefixQueue
+	q  *goque.Queue
+	pq *goque.PrefixQueue
 	rc int
 }
 
@@ -35,18 +36,29 @@ var (
 )
 
 type gq struct {
-	ctx        context.Context
-	qu         *goque.PrefixQueue
-	streamName string
+	ctx    context.Context
+	qu     *goque.Queue
+	pQu    *goque.PrefixQueue
+	prefix string
 }
 
 func (g *gq) Push(ctx context.Context, msg proto.Message) error {
-	_, er := g.qu.Enqueue([]byte(g.streamName), queue.EncodeProtoWithContext(ctx, msg))
+	var er error
+	if g.prefix != "" {
+		_, er = g.pQu.Enqueue([]byte(g.prefix), queue.EncodeProtoWithContext(ctx, msg))
+	} else {
+		_, er = g.qu.Enqueue(queue.EncodeProtoWithContext(ctx, msg))
+	}
 	return er
 }
 
 func (g *gq) PushRaw(_ context.Context, message broker.Message) error {
-	_, er := g.qu.Enqueue([]byte(g.streamName), queue.EncodeBrokerMessage(message))
+	var er error
+	if g.prefix != "" {
+		_, er = g.pQu.Enqueue([]byte(g.prefix), queue.EncodeBrokerMessage(message))
+	} else {
+		_, er = g.qu.Enqueue(queue.EncodeBrokerMessage(message))
+	}
 	return er
 }
 
@@ -55,18 +67,24 @@ func (g *gq) Consume(callback func(...broker.Message)) error {
 		for {
 			select {
 			case <-g.ctx.Done():
-				log.Logger(g.ctx).Debug("[goque] Closing consumer on context done" + g.streamName)
+				log.Logger(g.ctx).Debug("[goque] Closing consumer on context done" + g.prefix)
 				return
 			default:
 			}
-			it, er := g.qu.Dequeue([]byte(g.streamName))
+			var it *goque.Item
+			var er error
+			if g.prefix != "" {
+				it, er = g.pQu.Dequeue([]byte(g.prefix))
+			} else {
+				it, er = g.qu.Dequeue()
+			}
 			if er != nil {
 				if er == goque.ErrDBClosed {
-					log.Logger(g.ctx).Debug("[goque] Closing consumer on DB closed" + g.streamName)
+					log.Logger(g.ctx).Debug("[goque] Closing consumer on DB closed" + g.prefix)
 					return
 				}
 				if er != goque.ErrEmpty && er != goque.ErrOutOfBounds {
-					log.Logger(g.ctx).Error("[goque] Received error while consuming messages"+g.streamName, zap.Error(er))
+					log.Logger(g.ctx).Error("[goque] Received error while consuming messages"+g.prefix, zap.Error(er))
 				}
 				<-time.After(500 * time.Millisecond)
 				continue
@@ -74,7 +92,7 @@ func (g *gq) Consume(callback func(...broker.Message)) error {
 			if msg, er := queue.DecodeToBrokerMessage(it.Value); er == nil {
 				callback(msg)
 			} else {
-				log.Logger(g.ctx).Error("[goque] Cannot decode message in "+g.streamName, zap.Error(er))
+				log.Logger(g.ctx).Error("[goque] Cannot decode message in "+g.prefix, zap.Error(er))
 			}
 		}
 	}()
@@ -90,31 +108,53 @@ func (g *gq) OpenURL(ctx context.Context, u *url.URL) (queue.Queue, error) {
 	if streamName == "" {
 		return nil, fmt.Errorf("please provide a stream name")
 	}
+	queueName := "fifo-" + streamName
+	prefix := u.Query().Get("prefix")
+
 	ql.Lock()
-	if q, ok := queues[srv]; ok {
+	if q, ok := queues[queueName]; ok {
 		q.rc++
 		ql.Unlock()
-		return &gq{qu: q.q, streamName: streamName, ctx: ctx}, nil
+		return &gq{
+			ctx:    ctx,
+			qu:     q.q,
+			pQu:    q.pq,
+			prefix: prefix,
+		}, nil
 	}
 
 	srvDir := config2.MustServiceDataDir(srv)
-	q, err := goque.OpenPrefixQueue(filepath.Join(srvDir, "fifo"))
+	dataDir := filepath.Join(srvDir, queueName)
+	var sq *serviceQueue
+	var pq *goque.PrefixQueue
+	var q *goque.Queue
+	var err error
+	if prefix != "" {
+		pq, err = goque.OpenPrefixQueue(dataDir)
+	} else {
+		q, err = goque.OpenQueue(filepath.Join(srvDir, queueName))
+	}
+	sq = &serviceQueue{q: q, pq: pq, rc: 0}
 	if err != nil {
 		return nil, err
 	}
-	sq := &serviceQueue{q: q, rc: 0}
-	queues[srv] = sq
+	queues[queueName] = sq
 	ql.Unlock()
 	go func() {
 		<-ctx.Done()
 		sq.rc--
 		if sq.rc == 0 {
-			_ = q.Close()
+			if pq != nil {
+				_ = pq.Close()
+			} else {
+				_ = q.Close()
+			}
 		}
 	}()
 	return &gq{
-		ctx:        ctx,
-		qu:         q,
-		streamName: streamName,
+		ctx:    ctx,
+		qu:     q,
+		pQu:    pq,
+		prefix: prefix,
 	}, nil
 }
