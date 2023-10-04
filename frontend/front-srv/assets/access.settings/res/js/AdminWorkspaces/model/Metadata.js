c19e29236ab51315e28e7867da5bb855a40286cb
charles <charles@pydio.com>
2023-10-04 16:11:28 +0200

    Custom buttons in Metadata Board

--- frontend/front-srv/assets/access.settings/res/js/AdminWorkspaces/model/Metadata.js
+++ frontend/front-srv/assets/access.settings/res/js/AdminWorkspaces/model/Metadata.js
@@ -1,6 +1,24 @@
+/*
+ * Copyright 2023 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+ * This file is part of Pydio.
+ *
+ * Pydio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pydio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * The latest code can be found at <https://pydio.com>.
+ */
 
 import PydioApi from 'pydio/http/api'
-import ResourcesManager from 'pydio/http/resources-manager'
 import {UserMetaServiceApi, IdmUserMetaNamespace, IdmUpdateUserMetaNamespaceRequest, UpdateUserMetaNamespaceRequestUserMetaNsOp} from 'cells-sdk'
 
 class Metadata {
