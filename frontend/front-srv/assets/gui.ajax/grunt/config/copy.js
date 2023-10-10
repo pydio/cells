b376ad47a2cee641638877751f6184c88b8dfad9
charles <charles@pydio.com>
2023-10-10 11:17:10 +0200

    Remove refs to grunt

--- frontend/front-srv/assets/gui.ajax/grunt/config/copy.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    dndpatch: {
-        expand: true,
-        src: 'res/js/vendor/dnd-html5-backend-patch/NativeDragSources.js',
-        dest: 'node_modules/react-dnd-html5-backend/lib/',
-        flatten:true
-    }
-};
