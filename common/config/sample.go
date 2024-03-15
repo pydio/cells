31d1ff4b355d6b414ccf35cc49c818be692c4b67
charles <charles@pydio.com>
2024-03-15 11:15:08 +0100

    Update pydio.grpc.search defaults

--- common/config/sample.go
+++ common/config/sample.go
@@ -97,7 +97,9 @@ var SampleConfig = `{
         "pydio.grpc.search": {
             "indexContent": false,
 			"basenameAnalyzer": "standard",
-			"contentAnalyzer": "en"
+			"contentAnalyzer": "en",
+			"contentRef": "pydio:ContentRef",
+			"plainTextExtensions": "text,md"
         },
 		"pydio.grpc.policy": {
 			"dsn": "databaseParseTime"
