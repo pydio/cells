434e17fb54d0b6384143b63292c9694a3f0a4761
charles <charles@pydio.com>
2024-04-30 16:32:26 +0200

    Fix test

--- common/sql/resources/dao_test.go
+++ common/sql/resources/dao_test.go
@@ -24,57 +24,48 @@ import (
 	"context"
 	"testing"
 
-	. "github.com/smartystreets/goconvey/convey"
-	gsqlite "gorm.io/driver/sqlite"
-	"gorm.io/gorm"
-
-	"github.com/pydio/cells/v4/common/dao"
 	"github.com/pydio/cells/v4/common/dao/sqlite"
 	"github.com/pydio/cells/v4/common/proto/service"
+	"github.com/pydio/cells/v4/common/runtime/manager"
+	"github.com/pydio/cells/v4/common/utils/test"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+var (
+	testcases = []test.StorageTestCase{
+		{[]string{sqlite.Driver + "://" + sqlite.SharedMemDSN}, true, NewDAO},
+	}
 )
 
 func TestQueryResourceForAction(t *testing.T) {
 
-	wrapper := func(ctx context.Context, d dao.DAO) (dao.DAO, error) {
-		dialector := gsqlite.Open(d.DSN())
-		db, err := gorm.Open(dialector, &gorm.Config{
-			//DisableForeignKeyConstraintWhenMigrating: true,
-			FullSaveAssociations: true,
+	test.RunStorageTests(testcases, func(ctx context.Context) {
+
+		Convey("Test Query Builder", t, func() {
+
+			resDAO, err := manager.Resolve[DAO](ctx)
+			So(err, ShouldBeNil)
+
+			expr, e := resDAO.BuildPolicyConditionForAction(ctx, &service.ResourcePolicyQuery{Subjects: []string{"subject-1", "subject-2"}}, service.ResourcePolicyAction_READ)
+			So(e, ShouldBeNil)
+			So(expr, ShouldNotBeNil)
+			//So(queryString, ShouldEqual, "EXISTS ( select 1 from _policies WHERE (_policies.subject='subject-1' OR _policies.subject='subject-2') AND _policies.action='READ' AND _policies.resource=left.uuid )")
+
+			expr, e = resDAO.BuildPolicyConditionForAction(ctx, &service.ResourcePolicyQuery{Subjects: []string{}, Empty: true}, service.ResourcePolicyAction_READ)
+			So(e, ShouldBeNil)
+			So(expr, ShouldNotBeNil)
+			//So(queryString, ShouldEqual, "NOT EXISTS ( select 1 from _policies WHERE _policies.resource=left.uuid AND _policies.action='READ' )")
+
+			expr, e = resDAO.BuildPolicyConditionForAction(ctx, &service.ResourcePolicyQuery{Subjects: []string{}, Empty: false}, service.ResourcePolicyAction_READ)
+			So(e, ShouldBeNil)
+			So(expr, ShouldNotBeNil)
+			//So(queryString, ShouldEqual, "EXISTS ( select 1 from _policies WHERE _policies.action='READ' AND _policies.resource=left.uuid )")
+
+			expr, e = resDAO.BuildPolicyConditionForAction(ctx, &service.ResourcePolicyQuery{Subjects: []string{}, Any: true}, service.ResourcePolicyAction_READ)
+			So(e, ShouldBeNil)
+			So(expr, ShouldBeNil)
+
 		})
-		if err != nil {
-			return nil, err
-		}
-		return &ResourcesGORM{DB: db}, nil
-	}
-	ctx := context.Background()
-
-	d, e := dao.InitDAO(ctx, sqlite.Driver, sqlite.SharedMemDSN, "", wrapper)
-	if e != nil {
-		panic(e)
-	}
-	resDAO := d.(*ResourcesGORM)
-
-	Convey("Test Query Builder", t, func() {
-
-		expr, e := resDAO.BuildPolicyConditionForAction(&service.ResourcePolicyQuery{Subjects: []string{"subject-1", "subject-2"}}, service.ResourcePolicyAction_READ)
-		So(e, ShouldBeNil)
-		So(expr, ShouldNotBeNil)
-		//So(queryString, ShouldEqual, "EXISTS ( select 1 from _policies WHERE (_policies.subject='subject-1' OR _policies.subject='subject-2') AND _policies.action='READ' AND _policies.resource=left.uuid )")
-
-		expr, e = resDAO.BuildPolicyConditionForAction(&service.ResourcePolicyQuery{Subjects: []string{}, Empty: true}, service.ResourcePolicyAction_READ)
-		So(e, ShouldBeNil)
-		So(expr, ShouldNotBeNil)
-		//So(queryString, ShouldEqual, "NOT EXISTS ( select 1 from _policies WHERE _policies.resource=left.uuid AND _policies.action='READ' )")
-
-		expr, e = resDAO.BuildPolicyConditionForAction(&service.ResourcePolicyQuery{Subjects: []string{}, Empty: false}, service.ResourcePolicyAction_READ)
-		So(e, ShouldBeNil)
-		So(expr, ShouldNotBeNil)
-		//So(queryString, ShouldEqual, "EXISTS ( select 1 from _policies WHERE _policies.action='READ' AND _policies.resource=left.uuid )")
-
-		expr, e = resDAO.BuildPolicyConditionForAction(&service.ResourcePolicyQuery{Subjects: []string{}, Any: true}, service.ResourcePolicyAction_READ)
-		So(e, ShouldBeNil)
-		So(expr, ShouldBeNil)
-
 	})
-
 }
