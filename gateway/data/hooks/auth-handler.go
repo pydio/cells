4c199a278602f6f37c81a02521100e9a36cb2d9f
cdujeu <charles@pydio.com>
2025-01-07 14:45:55 +0100

    Refactor s3 gateway auth handler and use signature v4 for js presigned

--- gateway/data/hooks/auth-handler.go
+++ gateway/data/hooks/auth-handler.go
@@ -22,87 +22,55 @@ import (
 	"github.com/pydio/cells/v5/common/utils/propagator"
 )
 
-// authHandler - handles all the incoming authorization headers and validates them if possible.
+// pydioAuthHandler - handles all the incoming authorization headers and validates them if possible.
 type pydioAuthHandler struct {
-	rootCtx         context.Context
-	handler         http.Handler
-	jwtVerifier     *auth.JWTVerifier
-	globalAccessKey string
+	rootCtx      context.Context
+	handler      http.Handler
+	jwtVerifier  *auth.JWTVerifier
+	accessKey    string
+	accessSecret string
+	region       string
 }
 
 // GetPydioAuthHandlerFunc validates Pydio authorization headers for the incoming request.
-func GetPydioAuthHandlerFunc(ctx context.Context, globalAccessKey string) mux.MiddlewareFunc {
+func GetPydioAuthHandlerFunc(ctx context.Context, accessKey, secret, region string) mux.MiddlewareFunc {
 	return func(h http.Handler) http.Handler {
-		return pydioAuthHandler{
-			rootCtx:         ctx,
-			handler:         h,
-			jwtVerifier:     auth.DefaultJWTVerifier(),
-			globalAccessKey: globalAccessKey,
+		return &pydioAuthHandler{
+			rootCtx:      ctx,
+			handler:      h,
+			jwtVerifier:  auth.DefaultJWTVerifier(),
+			accessKey:    accessKey,
+			accessSecret: secret,
+			region:       region,
 		}
 	}
 }
 
 // handler for validating incoming authorization headers.
-func (a pydioAuthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (a *pydioAuthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	//var md map[string]string
 	var userName string
 
+	ctx := r.Context()
+	reqURL := r.URL
 	r, er := middleware.ApplyHTTPIncomingContextModifiers(r)
 	if er != nil {
-		w.WriteHeader(http.StatusBadRequest)
+		cmd.ExposedWriteErrorResponse(ctx, w, cmd.ErrBadRequest, reqURL)
 		return
 	}
 
-	ctx := propagator.ForkContext(r.Context(), a.rootCtx)
+	ctx = propagator.ForkContext(r.Context(), a.rootCtx)
 	ctx = runtime.WithServiceName(ctx, common.ServiceGatewayData)
 	r = r.WithContext(ctx)
 
-	resignRequestV4 := false
-	resignRequestV4Presigned := false
+	accessToken, doSign, doPreSign := a.extractToken(ctx, r)
 
-	rq := r.URL.Query()
-	jwt := rq.Get("pydio_jwt")
+	if accessToken != "" {
 
-	if len(jwt) > 0 {
-		//logger.Info("Found JWT in URL: replace by header and remove from URL")
-		r.Header.Set("X-Pydio-Bearer", jwt)
-		rq.Del("pydio_jwt")
-		checkResignV4 := false
-		if r.Method == http.MethodGet {
-			// Force attachment (if not already set)
-			if !strings.HasPrefix(strings.TrimSpace(rq.Get("response-content-disposition")), "attachment") {
-				rq.Set("response-content-disposition", "attachment")
-				checkResignV4 = true
-			}
-		}
-		// Rebuild Query
-		r.URL.RawQuery = rq.Encode()
-		_ = r.ParseForm()
-		// If PresignedV4, flag for re-signing
-		if signedKey, err := cmd.ExposedParsePresignV4(ctx, r.Form); err == nil && (signedKey != a.globalAccessKey || checkResignV4) {
-			resignRequestV4Presigned = true
-		}
-
-	} else if bearer, ok := r.Header["X-Pydio-Bearer"]; !ok || len(bearer) == 0 {
-		// Copy request.
-		req := *r
-		// Save authorization header.
-		v4Auth := req.Header.Get("Authorization")
-		// Parse signature version '4' header.
-		if signedKey, err := cmd.ExposedParseSignV4(ctx, v4Auth); err == nil && signedKey != a.globalAccessKey {
-			log.Logger(ctx).Debug("Use AWS Api Key as JWT: " + signedKey)
-			resignRequestV4 = true
-			r.Header.Set("X-Pydio-Bearer", signedKey)
-		}
-	}
-
-	if bearer, ok := r.Header["X-Pydio-Bearer"]; ok && len(bearer) > 0 {
-
-		rawIDToken := strings.Join(bearer, "")
 		var err error
 		var claims claim.Claims
-		ctx, claims, err = a.jwtVerifier.Verify(ctx, rawIDToken)
+		ctx, claims, err = a.jwtVerifier.Verify(ctx, accessToken)
 		if err != nil {
 			if amzDate := r.Header.Get("X-Amz-Date"); amzDate != "" {
 				// Parse header as ISO8861 format - It is set at signature time, so more or less represents the request issue time.
@@ -120,59 +88,21 @@ func (a pydioAuthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		userName = claims.Name
-		if resignRequestV4 {
-			// User is OK, override signature with service account ID/Secret
-			r = signer.SignV4(*r, common.S3GatewayRootUser, common.S3GatewayRootPassword, "", common.S3GatewayDefaultRegion)
-		} else if resignRequestV4Presigned {
-			origUrl := r.URL
-			origHeader := r.Header
-			newUrl := &url.URL{
-				Scheme: origUrl.Scheme,
-				Host:   origUrl.Host,
-				Path:   origUrl.Path,
-			}
-			newV := newUrl.Query()
-			expire := int64(900)
-			for k, vv := range origUrl.Query() {
-				if k == "X-Amz-Expires" {
-					if ex, e := strconv.ParseInt(strings.Join(vv, ""), 10, 64); e == nil {
-						expire = ex
-					}
-				}
-				if strings.HasPrefix(k, "X-Amz-") || k == "pydio_jwt" {
-					continue
-				}
-				for _, v := range vv {
-					newV.Set(k, v)
-				}
-			}
-			if len(newV) > 0 {
-				newUrl.RawQuery = newV.Encode()
-			}
 
-			newReq := *r
-			newReq.URL = newUrl
-			newReq.RequestURI = newUrl.RequestURI()
-			_ = newReq.ParseForm()
-			r = signer.PreSignV4(newReq, common.S3GatewayRootUser, common.S3GatewayRootPassword, "", common.S3GatewayDefaultRegion, expire)
-			// Re-append headers
-			if len(origHeader) > 0 {
-				r.Header = http.Header{}
-				for k, vv := range origHeader {
-					for _, v := range vv {
-						r.Header.Set(k, v)
-					}
-				}
-			}
+		if doSign {
+
+			r = signer.SignV4(*r, a.accessKey, a.accessSecret, "", a.region)
+
+		} else if doPreSign {
+
+			r = a.recomputePreSignV4(r)
+
 		}
-
-	} else if agent, aOk := r.Header["User-Agent"]; aOk && strings.Contains(strings.Join(agent, ""), "pydio.sync.client.s3") {
-
-		userName = common.PydioSystemUsername
 
 	} else {
 
 		if user, er := permissions.SearchUniqueUser(ctx, common.PydioS3AnonUsername, ""); er == nil {
+
 			userName = common.PydioS3AnonUsername
 			var s []string
 			for _, role := range user.Roles {
@@ -189,8 +119,10 @@ func (a pydioAuthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			ctx = context.WithValue(ctx, claim.ContextKey, anonClaim)
 
 		} else {
+
 			cmd.ExposedWriteErrorResponse(ctx, w, cmd.ErrAccessDenied, r.URL)
 			return
+
 		}
 
 	}
@@ -199,4 +131,102 @@ func (a pydioAuthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	newRequest := r.WithContext(ctx)
 	a.handler.ServeHTTP(w, newRequest)
 
+}
+
+func (a *pydioAuthHandler) extractToken(ctx context.Context, r *http.Request) (token string, doSign bool, doPresign bool) {
+
+	rq := r.URL.Query()
+
+	if bearer := r.Header.Get("X-Pydio-Bearer"); bearer != "" {
+
+		token = bearer
+		r.Header.Del("X-Pydio-Bearer")
+
+	} else if jwt := rq.Get("pydio_jwt"); len(jwt) > 0 {
+
+		//logger.Info("Found JWT in URL: replace by header and remove from URL")
+		token = jwt
+
+		rq.Del("pydio_jwt")
+		checkResignV4 := false
+		if r.Method == http.MethodGet {
+			// Force attachment (if not already set)
+			if !strings.HasPrefix(strings.TrimSpace(rq.Get("response-content-disposition")), "attachment") {
+				rq.Set("response-content-disposition", "attachment")
+				checkResignV4 = true
+			}
+		}
+		// Rebuild Query
+		r.URL.RawQuery = rq.Encode()
+		_ = r.ParseForm()
+		// If PresignedV4, flag for re-signing
+		if signedKey, err := cmd.ExposedParsePresignV4(ctx, r.Form); err == nil && (signedKey != a.accessKey || checkResignV4) {
+			doPresign = true
+		}
+
+	} else {
+
+		if v4Auth := r.Header.Get("Authorization"); v4Auth == "" {
+			_ = r.ParseForm()
+			if signedKey, err := cmd.ExposedParsePresignV4(ctx, r.Form); err == nil && signedKey != a.accessKey {
+				token = signedKey
+				doPresign = true
+			}
+
+		} else if signedKey, err := cmd.ExposedParseSignV4(ctx, v4Auth); err == nil && signedKey != a.accessKey {
+			// Parse signature version '4' header.
+			log.Logger(ctx).Debug("Use AWS Api Key as JWT: " + signedKey)
+			token = signedKey
+			doSign = true
+		}
+
+	}
+
+	return
+}
+
+func (a *pydioAuthHandler) recomputePreSignV4(r *http.Request) *http.Request {
+
+	origUrl := r.URL
+	origHeader := r.Header
+	newUrl := &url.URL{
+		Scheme: origUrl.Scheme,
+		Host:   origUrl.Host,
+		Path:   origUrl.Path,
+	}
+	newV := newUrl.Query()
+	expire := int64(900)
+	for k, vv := range origUrl.Query() {
+		if k == "X-Amz-Expires" {
+			if ex, e := strconv.ParseInt(strings.Join(vv, ""), 10, 64); e == nil {
+				expire = ex
+			}
+		}
+		if strings.HasPrefix(k, "X-Amz-") || k == "pydio_jwt" {
+			continue
+		}
+		for _, v := range vv {
+			newV.Set(k, v)
+		}
+	}
+	if len(newV) > 0 {
+		newUrl.RawQuery = newV.Encode()
+	}
+
+	newReq := *r
+	newReq.URL = newUrl
+	newReq.RequestURI = newUrl.RequestURI()
+	_ = newReq.ParseForm()
+	signed := signer.PreSignV4(newReq, a.accessKey, a.accessSecret, "", a.region, expire)
+	// Re-append headers
+	if len(origHeader) > 0 {
+		signed.Header = http.Header{}
+		for k, vv := range origHeader {
+			for _, v := range vv {
+				signed.Header.Set(k, v)
+			}
+		}
+	}
+
+	return signed
 }
