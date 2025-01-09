78d1c2e94a5e6fad1903c6b5556c9e8e346396ea
cdujeu <charles@pydio.com>
2025-01-09 10:57:27 +0100

    gorm signature change

--- common/storage/sql/dbresolver/dbresolver.go
+++ common/storage/sql/dbresolver/dbresolver.go
@@ -170,10 +170,9 @@ func (dr *DBResolver) convertToConnPool(dialectors []gorm.Dialector) (connPools 
 			}
 
 			dr.prepareStmtStore[connPool] = &gorm.PreparedStmtDB{
-				ConnPool:    db.Config.ConnPool,
-				Stmts:       map[string]*gorm.Stmt{},
-				Mux:         &sync.RWMutex{},
-				PreparedSQL: make([]string, 0, 100),
+				ConnPool: db.Config.ConnPool,
+				Stmts:    map[string]*gorm.Stmt{},
+				Mux:      &sync.RWMutex{},
 			}
 
 			connPools = append(connPools, connPool)
