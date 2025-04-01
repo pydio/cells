9932624fe3bc3dc048ee0f465892218cc2af550c
Tran The Cuong <c12simple@users.noreply.github.com>
2025-04-01 18:15:40 +0200

    Wopi - Fix lastModifyTime and X-COOL-WOPI-Timestamp check

--- gateway/wopi/file.go
+++ gateway/wopi/file.go
@@ -110,6 +110,25 @@ func uploadStream(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Check if LastModifiedTime changed, ask user to resolve the conflict
+	coolTimeStr := r.Header.Get("X-COOL-WOPI-Timestamp")
+	if coolTimeStr != "" {
+		coolTime, err := time.Parse(time.RFC3339, coolTimeStr)
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		if !coolTime.Equal(n.GetModTime()) {
+			w.WriteHeader(http.StatusConflict)
+			json.NewEncoder(w).Encode(map[string]interface{}{
+				"COOLStatusCode": 1010,
+			})
+			return
+		}
+	}
+
 	var size int64
 	if h, ok := r.Header["Content-Length"]; ok && len(h) > 0 {
 		size, _ = strconv.ParseInt(h[0], 10, 64)
@@ -129,7 +148,19 @@ func uploadStream(w http.ResponseWriter, r *http.Request) {
 	}
 
 	log.Logger(r.Context()).Debug("uploaded node", n.Zap(), zap.Int64("Data Length", written.Size))
+
+	// Readnode for info
+	n, err = findNodeFromRequest(r)
+
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
 	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"LastModifiedTime": n.GetModTime().Format(time.RFC3339),
+	})
 }
 
 func buildFileFromNode(ctx context.Context, n *tree.Node) *File {
@@ -148,7 +179,11 @@ func buildFileFromNode(ctx context.Context, n *tree.Node) *File {
 		if claims, ok := cValue.(claim.Claims); ok {
 
 			f.UserId = claims.Name
-			f.UserFriendlyName = claims.DisplayName
+			if claims.DisplayName == "" {
+				f.UserFriendlyName = claims.Name
+			} else {
+				f.UserFriendlyName = claims.DisplayName
+			}
 
 			pydioReadOnly := n.GetStringMeta(common.MetaFlagReadonly)
 			if pydioReadOnly == "true" {
