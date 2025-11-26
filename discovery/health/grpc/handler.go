6111b155da63176fca144bad0c4b77b9cf5841cf
cdujeu <charles@pydio.com>
2025-11-26 17:48:29 +0100

    chore(deps): update HealthServer interface

--- discovery/health/grpc/handler.go
+++ discovery/health/grpc/handler.go
@@ -3,11 +3,17 @@ package grpc
 import (
 	"context"
 	"fmt"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	"google.golang.org/grpc/health/grpc_health_v1"
 )
 
 type Handler struct{}
+
+func (h *Handler) List(ctx context.Context, request *grpc_health_v1.HealthListRequest) (*grpc_health_v1.HealthListResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "not implemented")
+}
 
 func (h *Handler) Check(ctx context.Context, req *grpc_health_v1.HealthCheckRequest) (*grpc_health_v1.HealthCheckResponse, error) {
 	fmt.Println("health checking")
