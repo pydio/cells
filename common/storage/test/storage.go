e312411e6739691fb96b628070ffd316665dfe84
cdujeu <charles@pydio.com>
2025-12-17 15:38:39 +0100

    fix(tests): revert changes in test storage

--- common/storage/test/storage.go
+++ common/storage/test/storage.go
@@ -65,7 +65,7 @@ func TemplateSQL(daoFunc any) []StorageTestCase {
 	if other := os.Getenv("CELLS_TEST_MYSQL_DSN"); other != "" {
 		for _, dsn := range strings.Split(other, ";") {
 			ss = append(ss, StorageTestCase{
-				DSN:       []string{strings.TrimSpace(dsn) + "?parseTime=true&tls=false&hookNames=cleanTables&prefix=test_" + unique},
+				DSN:       []string{strings.TrimSpace(dsn) + "?parseTime=true&hookNames=cleanTables&prefix=test_" + unique},
 				Condition: true,
 				DAO:       daoFunc,
 			})
@@ -96,7 +96,7 @@ func TemplateSQLService(services map[string]map[string]map[string]any) []Service
 	if other := os.Getenv("CELLS_TEST_MYSQL_DSN"); other != "" {
 		for _, dsn := range strings.Split(other, ";") {
 			ss = append(ss, ServicesStorageTestCase{
-				DSN:       map[string]string{"sql": strings.TrimSpace(dsn) + "?parseTime=true&tls=false&hookNames=cleanTables&prefix=test_" + unique},
+				DSN:       map[string]string{"sql": strings.TrimSpace(dsn) + "?parseTime=true&hookNames=cleanTables&prefix=test_" + unique},
 				Condition: true,
 				Services:  services,
 			})
