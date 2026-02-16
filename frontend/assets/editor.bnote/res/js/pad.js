8a2b70e910adc9560d624f0600e9eb93655e072b
Charles du Jeu <charles@pydio.com>
2026-02-16 12:43:08 +0100

    fix(pages): fix styling broken after last update, improve code blocks, tables, remove full page display for now. (#819)
    * fix(pages): fix styling broken after last update, improve code blocks, tables, remove full page display for now.
    
    * fix(pages): update axios library

--- frontend/assets/editor.bnote/res/js/pad.js
+++ frontend/assets/editor.bnote/res/js/pad.js
@@ -17,10 +17,12 @@
  *
  * The latest code can be found at <https://pydio.com>.
  */
-import React, {useState, useEffect, useCallback, useMemo} from 'react'
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import Pydio from 'pydio';
-import "@blocknote/core/fonts/inter.css";
-import { BlockNoteView } from "@blocknote/mantine";
+// Default styles for the mantine editor
+import '@blocknote/mantine/style.css';
+import '@blocknote/core/fonts/inter.css';
+import { BlockNoteView } from '@blocknote/mantine';
 import {
     useCreateBlockNote,
     getDefaultReactSlashMenuItems,
@@ -28,114 +30,159 @@ import {
     AddBlockButton,
     SideMenu,
     SideMenuController,
-} from "@blocknote/react";
-import { BlockNoteSchema, defaultBlockSpecs, defaultInlineContentSpecs } from "@blocknote/core";
-import { filterSuggestionItems } from "@blocknote/core/extensions";
-import { en } from '@blocknote/core/locales'
+} from '@blocknote/react';
+import {
+    BlockNoteSchema,
+    defaultBlockSpecs,
+    createCodeBlockSpec,
+    defaultInlineContentSpecs,
+} from '@blocknote/core';
+import { filterSuggestionItems } from '@blocknote/core/extensions';
+import { en } from '@blocknote/core/locales';
+// This packages some of the most used languages in on-demand bundle
+import { codeBlockOptions } from './blocks/codeblock';
 
-import {MentionSuggestionMenu, mentionInlineSpecs} from './specs/Mention'
+import { MentionSuggestionMenu, mentionInlineSpecs } from './specs/Mention';
 import {
     nodeBlockSpecs,
     nodeInlineSpecs,
     insertChildrenList,
     insertNodePickerBlock,
     insertResultsList,
-    pasteHandler
-} from "./specs/NodeRef";
-import {alertBlockSpecs, insertAlertItem} from './specs/Alert'
-import {insertSubPageItem} from "./specs/SubPage";
-import {SideMenuButton} from "./SideMenuButton";
-import ContextMenuModel from 'pydio/model/context-menu'
-import {headerBlockSpecs, HeaderSpecType} from "./specs/Header";
-import {findExistingHeader} from "./hooks/useNodeTitle";
+    pasteHandler,
+} from './specs/NodeRef';
+import { alertBlockSpecs, insertAlertItem } from './specs/Alert';
+import { insertSubPageItem } from './specs/SubPage';
+import { SideMenuButton } from './SideMenuButton';
+import ContextMenuModel from 'pydio/model/context-menu';
+import { headerBlockSpecs, HeaderSpecType } from './specs/Header';
+import { findExistingHeader } from './hooks/useNodeTitle';
 
-import {padFileDropHandler} from "./hooks/padFileDropHandler";
-import './pad-styles.less'
-const {ModalSearch} = Pydio.requireLib('workspaces');
-const {emptyDataModel} = Pydio.requireLib('hoc')
-
+import { padFileDropHandler } from './hooks/padFileDropHandler';
+import './pad-styles.less';
+const { ModalSearch } = Pydio.requireLib('workspaces');
+const { emptyDataModel } = Pydio.requireLib('hoc');
 
 const schema = BlockNoteSchema.create({
     blockSpecs: {
         ...defaultBlockSpecs,
         ...nodeBlockSpecs,
         ...alertBlockSpecs,
-        ...headerBlockSpecs
+        ...headerBlockSpecs,
+        codeBlock: createCodeBlockSpec(codeBlockOptions),
     },
     inlineContentSpecs: {
         ...defaultInlineContentSpecs,
         ...nodeInlineSpecs,
         ...mentionInlineSpecs,
-    }
+    },
 });
 
-const defaultExcludedKeys = ["audio", "video", "image", "file"]
+const defaultExcludedKeys = ['audio', 'video', 'image', 'file'];
 
 // List containing all default Slash Menu Items, as well as our custom one.
-const getCustomSlashMenuItems = (
-    editor
-) => [
-    ...getDefaultReactSlashMenuItems(editor).filter((item => defaultExcludedKeys.indexOf(item.key) === -1)),
-    insertChildrenList(editor),
-    insertNodePickerBlock(editor),
-    insertResultsList(editor),
-    insertAlertItem(editor),
-    insertSubPageItem(editor),
-];
+const getCustomSlashMenuItems = (editor) => {
+    const all = [
+        ...getDefaultReactSlashMenuItems(editor).filter(
+            (item) => defaultExcludedKeys.indexOf(item.key) === -1,
+        ),
+        insertChildrenList(editor),
+        insertNodePickerBlock(editor),
+        insertResultsList(editor),
+        insertAlertItem(editor),
+        insertSubPageItem(editor),
+    ];
+    // Ensure Groups ordering and grouping
+    const groupsOrder = [
+        'Basic blocks',
+        'Heading',
+        'Subheadings',
+        'Advanced',
+        'Others',
+    ];
+    let ordered = [];
+    groupsOrder.forEach((group) => {
+        ordered = [...ordered, ...all.filter((i) => i.group === group)];
+    });
+    return ordered;
+};
 
-export default ({initialContent = [], onChange, darkMode, readOnly, style}) => {
-
-    const [htmlReady, setHtmlReady] = useState('')
+export default ({
+    initialContent = [],
+    onChange,
+    darkMode,
+    readOnly,
+    style,
+}) => {
+    const [htmlReady, setHtmlReady] = useState('');
 
     // Creates a new editor instance.
     const editor = useCreateBlockNote({
         schema,
-        initialContent:initialContent.length?initialContent:null,
+        initialContent: initialContent.length ? initialContent : null,
         // We override the `placeholders` in our dictionary
         dictionary: {
             ...en,
             placeholders: {
                 ...en.placeholders,
                 // We override the default placeholder
-                default: "Type text or '/' for commands, '%' for mentioning a file, '@' for mentioning a user",
-            }
+                default:
+                    "Type text or '/' for commands, '@' for mentioning a user",
+            },
         },
         pasteHandler: pasteHandler,
-        setIdAttribute:true
+        setIdAttribute: true,
+        tables: {
+            splitCells: true,
+            cellBackgroundColor: true,
+            cellTextColor: true,
+            headers: true,
+        },
     });
 
-    const onChangePreventHeaderDelete = useCallback(()=>{
-        const blocks = editor.document
-        if(!findExistingHeader(blocks)){
-            editor.insertBlocks([{type:HeaderSpecType}], blocks && blocks.length ? blocks[0]: null, 'before')
-            return
+    const onChangePreventHeaderDelete = useCallback(() => {
+        const blocks = editor.document;
+        if (!findExistingHeader(blocks)) {
+            editor.insertBlocks(
+                [{ type: HeaderSpecType }],
+                blocks && blocks.length ? blocks[0] : null,
+                'before',
+            );
+            return;
         }
-        onChange(blocks)
-    }, [editor, onChange])
+        onChange(blocks);
+    }, [editor, onChange]);
 
-    const searchDM = useMemo(() => emptyDataModel(), [])
+    const searchDM = useMemo(() => emptyDataModel(), []);
 
     let main;
-    if(false && readOnly) {
+    if (false && readOnly) {
         useEffect(() => {
-            editor.blocksToFullHTML(initialContent||[]).then(res => setHtmlReady(res))
-        }, [initialContent])
-        if(htmlReady) {
-            main =(<div
-                data-color-scheme={darkMode?"dark":"light"}
-                className={"bn-container bn-readonly bn-editor bn-mantine bn-default-styles"}
-                dangerouslySetInnerHTML={{__html:htmlReady}}
-            />)
+            editor
+                .blocksToFullHTML(initialContent || [])
+                .then((res) => setHtmlReady(res));
+        }, [initialContent]);
+        if (htmlReady) {
+            main = (
+                <div
+                    data-color-scheme={darkMode ? 'dark' : 'light'}
+                    className={
+                        'bn-container bn-readonly bn-editor bn-mantine bn-default-styles'
+                    }
+                    dangerouslySetInnerHTML={{ __html: htmlReady }}
+                />
+            );
         } else {
-            main = 'Rendering HTML...'
+            main = 'Rendering HTML...';
         }
     } else {
+        const slashMenuItems = getCustomSlashMenuItems(editor);
         main = (
             <BlockNoteView
                 editable={!readOnly}
                 onChange={onChangePreventHeaderDelete}
                 editor={editor}
-                theme={darkMode?"dark":"light"}
+                theme={darkMode ? 'dark' : 'light'}
                 sideMenu={false}
                 slashMenu={false}
                 onClick={(e) => ContextMenuModel.getInstance().close()}
@@ -150,26 +197,39 @@ export default ({initialContent = [], onChange, darkMode, readOnly, style}) => {
                     )}
                 />
                 <SuggestionMenuController
-                    triggerCharacter={"/"}
+                    triggerCharacter={'/'}
                     // Replaces the default Slash Menu items with our custom ones.
                     getItems={async (query) =>
-                        filterSuggestionItems(getCustomSlashMenuItems(editor), query)
+                        filterSuggestionItems(slashMenuItems, query)
                     }
                 />
-                <MentionSuggestionMenu editor={editor}/>
+                <MentionSuggestionMenu editor={editor} />
             </BlockNoteView>
-        )
+        );
     }
 
     // Renders the editor instance using a React component.
     return (
-        <div style={{flex: 1, width: '100%', backgroundColor:'var(--md-sys-color-surface)', paddingTop: 20, userSelect:"inherit", ...style}}
-             onClick={(e) => e.stopPropagation()}
-             onKeyUp={(e) => e.stopPropagation()}
-             onDrop={(e) => padFileDropHandler(editor, e, true)}
+        <div
+            style={{
+                flex: 1,
+                width: '100%',
+                backgroundColor: 'var(--md-sys-color-surface)',
+                paddingTop: 20,
+                userSelect: 'inherit',
+                ...style,
+            }}
+            onClick={(e) => e.stopPropagation()}
+            onKeyUp={(e) => e.stopPropagation()}
+            onDrop={(e) => padFileDropHandler(editor, e, true)}
         >
             {main}
-            <ModalSearch pydio={Pydio.getInstance()} dataModel={searchDM} accessKey={''} eventName={'pydioOpenSelector'}/>
+            <ModalSearch
+                pydio={Pydio.getInstance()}
+                dataModel={searchDM}
+                accessKey={''}
+                eventName={'pydioOpenSelector'}
+            />
         </div>
     );
 };
