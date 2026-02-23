e9aaaf0bdae2a10a919137108bb99928c8075e43
Osama Salama <osama.salama@wire.com>
2026-02-23 09:28:37 +0100

    fix(namespace): Fix failing unit test
    - Json Schema Factory should not concat namespace title

--- idm/meta/json_schema/json_schema_test.go
+++ idm/meta/json_schema/json_schema_test.go
@@ -257,7 +257,7 @@ func TestJsonSchemaCoverage(t *testing.T) {
 	Convey("JSONSchemaFactory.BuildJsonSchema returns bytes and contains expected fields", t, func() {
 		f := NewJSONSchemaFactory("string")
 
-		bt, st := f.BuildJsonSchema("string", "string-label", "")
+		bt, st := f.BuildJsonSchema("string", "usermeta-text", "")
 		So(bt, ShouldNotBeNil)
 
 		var m map[string]interface{}
@@ -274,7 +274,7 @@ func TestJsonSchemaCoverage(t *testing.T) {
 		}
 
 		// other labels
-		bt2, _ := f.BuildJsonSchema("textarea", "textarea-label", "")
+		bt2, _ := f.BuildJsonSchema("textarea", "usermeta-long-text", "")
 		So(bt2, ShouldNotBeNil)
 		var mt map[string]interface{}
 		So(json.Unmarshal(bt2, &mt), ShouldBeNil)
