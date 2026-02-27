9e610e6c5d9b857e29d79ffb9e563eef8f7c6178
Osama Salama <4039929+osalama7@users.noreply.github.com>
2026-02-27 09:08:18 +0100

    fix(namespace): Add default read policy for namespace list for standard users (#843)

--- idm/policy/defaults.go
+++ idm/policy/defaults.go
@@ -170,6 +170,7 @@ var (
 					Subjects:    []string{"profile:standard", "profile:shared"},
 					Resources: []string{
 						"rest:/user-meta/bookmarks",
+						"rest:/user-meta/namespace",
 						"rest:/user-meta/namespace/<.+>",
 						"rest:/user-meta/search",
 						"rest:/user-meta/tags/<.+>",
@@ -543,6 +544,7 @@ func Upgrade210(ctx context.Context) error {
 					Subjects:    []string{"profile:standard", "profile:shared"},
 					Resources: []string{
 						"rest:/user-meta/bookmarks",
+						"rest:/user-meta/namespace",
 						"rest:/user-meta/namespace/<.+>",
 						"rest:/user-meta/search",
 						"rest:/user-meta/tags/<.+>",
