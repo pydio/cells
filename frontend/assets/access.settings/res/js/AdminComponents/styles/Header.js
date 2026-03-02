1b7674486a183581132f3acb48e4fe8a9385c94a
Charles du Jeu <charles@pydio.com>
2026-03-02 09:55:27 +0100

    fix(styles): minor glitches after recent pruning of the core reset.css (#845)

--- frontend/assets/access.settings/res/js/AdminComponents/styles/Header.js
+++ frontend/assets/access.settings/res/js/AdminComponents/styles/Header.js
@@ -18,59 +18,68 @@
  * The latest code can be found at <https://pydio.com>.
  */
 
-import {Component} from 'react'
-import {Paper, FontIcon, IconButton, RefreshIndicator} from 'material-ui'
-import {muiThemeable} from 'material-ui/styles'
-import DOMUtils from 'pydio/util/dom'
-import AdminStyles from "./AdminStyles";
-import {LeftToggleListener} from '../board/AdminDashboard'
+import { Component } from 'react';
+import { Paper, FontIcon, IconButton, RefreshIndicator } from 'material-ui';
+import { muiThemeable } from 'material-ui/styles';
+import DOMUtils from 'pydio/util/dom';
+import AdminStyles from './AdminStyles';
+import { LeftToggleListener } from '../board/AdminDashboard';
 
-class Header extends Component{
-
-    render(){
-
-        const {reloadAction, loading, backButtonAction, scrolling,
-            title, centerContent, actions, tabs, tabValue, onTabChange, muiTheme, editorMode} = this.props;
+class Header extends Component {
+    render() {
+        const {
+            reloadAction,
+            loading,
+            backButtonAction,
+            scrolling,
+            title,
+            centerContent,
+            actions,
+            tabs,
+            tabValue,
+            onTabChange,
+            muiTheme,
+            editorMode,
+        } = this.props;
 
         const adminStyles = AdminStyles(muiTheme.palette);
         const listener = LeftToggleListener.getInstance();
 
         let styles = {
             base: {
-                padding: listener.isActive() ? '0 16px 0 0':'0 16px',
+                padding: listener.isActive() ? '0 16px 0 0' : '0 16px',
                 backgroundColor: '#ffffff',
                 boxShadow: 'rgba(0, 0, 0, 0.1) 0px 1px 2px',
                 zIndex: 10,
-                marginLeft:listener.isActive() && listener.isOpen() ? 256 : 0
+                marginLeft: listener.isActive() && listener.isOpen() ? 256 : 0,
             },
             container: {
-                display:'flex',
+                display: 'flex',
                 width: '100%',
                 padding: '0 12px',
                 height: 64,
-                alignItems: 'center'
+                alignItems: 'center',
             },
             title: {
                 fontSize: 18,
                 whiteSpace: 'nowrap',
                 overflow: 'hidden',
-                textOverflow: 'ellipsis'
+                textOverflow: 'ellipsis',
+                fontWeight: 400,
             },
-            legend: {
-
-            },
+            legend: {},
             icon: {
                 color: 'rgba(0,0,0,0.24)',
-                marginRight: 6
+                marginRight: 6,
             },
             refresh: {
                 display: 'inline-block',
                 position: 'relative',
                 //marginRight: 9,
-                marginLeft: 7
+                marginLeft: 7,
             },
-            tabs : {
-                tab : {
+            tabs: {
+                tab: {
                     fontSize: 15,
                     marginBottom: 0,
                     paddingBottom: 13,
@@ -79,21 +88,21 @@ class Header extends Component{
                     textTransform: 'uppercase',
                     fontWeight: 500,
                     cursor: 'pointer',
-                    whiteSpace:'nowrap',
+                    whiteSpace: 'nowrap',
                     color: 'rgba(0, 0, 0, 0.73)',
                     transition: DOMUtils.getBeziersTransition(),
-                    borderBottom: '2px solid transparent'
+                    borderBottom: '2px solid transparent',
                 },
                 tabIcon: {
                     fontSize: 14,
                     color: 'inherit',
-                    marginRight: 5
+                    marginRight: 5,
                 },
                 tabActive: {
                     borderBottom: '2px solid ' + muiTheme.palette.primary2Color,
                     color: muiTheme.palette.primary2Color,
-                }
-            }
+                },
+            },
         };
 
         styles.scrolling = {
@@ -106,90 +115,133 @@ class Header extends Component{
             right: 0,
         };
 
-        if (editorMode){
+        if (editorMode) {
             styles.base = {
                 ...styles.base,
                 backgroundColor: muiTheme.palette.primary1Color,
                 borderBottom: 0,
-                borderRadius:'2px 2px 0 0 '
+                borderRadius: '2px 2px 0 0 ',
             };
             styles.title = {
                 ...styles.title,
-                color: 'white'
+                color: 'white',
             };
             styles.container = {
                 ...styles.container,
-                height: 48
+                height: 48,
             };
             styles.scrolling = {
                 ...styles.base,
                 ...styles.scrolling,
                 backgroundColor: muiTheme.palette.primary1Color,
-            }
+            };
         }
 
-
-
         let icon;
-        if(listener.isActive()){
-            icon = <IconButton iconClassName={listener.isOpen()?"mdi mdi-backburger":"mdi mdi-menu"} iconStyle={styles.icon} onClick={() => listener.toggle()}/>
-        } else if(this.props.icon) {
-            icon = <FontIcon className={this.props.icon} style={styles.icon} />
-        } else if(backButtonAction) {
-            icon = <IconButton style={{marginLeft: -18}} iconClassName={"mdi mdi-chevron-left"} onClick={backButtonAction}/>
+        if (listener.isActive()) {
+            icon = (
+                <IconButton
+                    iconClassName={
+                        listener.isOpen()
+                            ? 'mdi mdi-backburger'
+                            : 'mdi mdi-menu'
+                    }
+                    iconStyle={styles.icon}
+                    onClick={() => listener.toggle()}
+                />
+            );
+        } else if (this.props.icon) {
+            icon = <FontIcon className={this.props.icon} style={styles.icon} />;
+        } else if (backButtonAction) {
+            icon = (
+                <IconButton
+                    style={{ marginLeft: -18 }}
+                    iconClassName={'mdi mdi-chevron-left'}
+                    onClick={backButtonAction}
+                />
+            );
         }
 
         let reloadButton;
-        if(reloadAction){
-            reloadButton = <IconButton iconClassName={"mdi mdi-reload"} onClick={reloadAction} {...adminStyles.props.header.iconButton} />;
+        if (reloadAction) {
+            reloadButton = (
+                <IconButton
+                    iconClassName={'mdi mdi-reload'}
+                    onClick={reloadAction}
+                    {...adminStyles.props.header.iconButton}
+                />
+            );
         }
 
         let headTitle = <h3 style={styles.title}>{title}</h3>;
         if (tabs) {
-            headTitle = <div style={{display:'flex', height:'100%', paddingTop: 20}}>{tabs.map(tab => {
-                let st = styles.tabs.tab;
-                if(tab.Value === tabValue){
-                    st = {...st, ...styles.tabs.tabActive};
-                }
-                const icon = tab.Icon ? <FontIcon className={tab.Icon} style={styles.tabs.tabIcon}/> : null;
-                return <h3 style={st} onClick={()=>onTabChange(tab.Value)}>{icon}{tab.Label}</h3>
-            })}</div>
+            headTitle = (
+                <div
+                    style={{ display: 'flex', height: '100%', paddingTop: 20 }}
+                >
+                    {tabs.map((tab) => {
+                        let st = styles.tabs.tab;
+                        if (tab.Value === tabValue) {
+                            st = { ...st, ...styles.tabs.tabActive };
+                        }
+                        const icon = tab.Icon ? (
+                            <FontIcon
+                                className={tab.Icon}
+                                style={styles.tabs.tabIcon}
+                            />
+                        ) : null;
+                        return (
+                            <h3
+                                style={st}
+                                onClick={() => onTabChange(tab.Value)}
+                            >
+                                {icon}
+                                {tab.Label}
+                            </h3>
+                        );
+                    })}
+                </div>
+            );
         }
         let actionButtons = actions;
-        if(!actionButtons){
-            actionButtons = []
-        } else if(!actionButtons.map){
+        if (!actionButtons) {
+            actionButtons = [];
+        } else if (!actionButtons.map) {
             actionButtons = [actionButtons];
         }
-        let finalStyle = scrolling?styles.scrolling:styles.base
-        finalStyle = {...finalStyle, ...this.props.style}
+        let finalStyle = scrolling ? styles.scrolling : styles.base;
+        finalStyle = { ...finalStyle, ...this.props.style };
 
         return (
-            <Paper style={finalStyle} zDepth={scrolling?1:0}>
+            <Paper style={finalStyle} zDepth={scrolling ? 1 : 0}>
                 <div style={styles.container}>
                     {icon}
                     {headTitle}
-                    <div style={{flex:1, marginRight:centerContent?8:0}}>{centerContent}</div>
-                    <div style={{display:'flex', alignItems:'center'}}>
-                        {actionButtons.map(a => <div style={{margin:0}}>{a}</div>)}
+                    <div
+                        style={{ flex: 1, marginRight: centerContent ? 8 : 0 }}
+                    >
+                        {centerContent}
+                    </div>
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                        {actionButtons.map((a) => (
+                            <div style={{ margin: 0 }}>{a}</div>
+                        ))}
                         {!loading && reloadButton}
-                        {loading &&
+                        {loading && (
                             <RefreshIndicator
                                 size={30}
                                 left={0}
                                 top={0}
-                                status='loading'
+                                status="loading"
                                 style={styles.refresh}
                             />
-                        }
+                        )}
                     </div>
                 </div>
             </Paper>
         );
-
     }
-
 }
 
 Header = muiThemeable()(Header);
-export {Header as default}
+export { Header as default };
