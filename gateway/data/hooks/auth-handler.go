dd3a8b143f3c0083c0a400fee2c046d0fb30a05d
Greg Hecquet <greg.tf.hecquet@gmail.com>
2026-05-29 09:05:14 +0200

    fix: restore pydio_jwt compatibility for presigned urls (used by ios) (#934)

--- gateway/data/hooks/auth-handler.go
+++ gateway/data/hooks/auth-handler.go
@@ -3,10 +3,13 @@ package hooks
 import (
 	"context"
 	"net/http"
+	"net/url"
+	"strconv"
 	"strings"
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/minio/minio-go/v7/pkg/signer"
 	"github.com/minio/minio/cmd"
 
 	"github.com/pydio/cells/v5/common"
@@ -56,10 +59,11 @@ func (a *pydioAuthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	ctx = runtime.WithServiceName(ctx, common.ServiceGatewayData)
 	r = r.WithContext(ctx)
 
-	accessToken, err := a.extractToken(ctx, r)
+	accessToken, fromPydioJWT, err := a.extractToken(ctx, r)
 	if err != nil {
 		log.Logger(ctx).Errorf("Error extracting token: %v", err)
 		cmd.ExposedWriteErrorResponse(ctx, w, cmd.ErrUnauthorizedAccess, reqURL)
+		return
 	}
 
 	if accessToken != "" {
@@ -84,12 +88,23 @@ func (a *pydioAuthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		}
 		userName = claims.Name
 
-		// We may now have an updated secret key inferred from accessToken - using global one for now
-		secret := common.S3GatewayRootPassword
-		if sp := claims.GetSecretPair(); sp != "" {
-			secret = sp
+		if fromPydioJWT {
+			// V4 accepted pydio_jwt on presigned S3 URLs. Strip it from the canonical query,
+			// verify the JWT, then re-sign with gateway credentials before MinIO validates it.
+			var ok bool
+			if r, ok = a.resignPydioJWTPresignedRequest(r); !ok {
+				cmd.ExposedWriteErrorResponse(ctx, w, cmd.ErrSignatureVersionNotSupported, r.URL)
+				return
+			}
+			ctx = cmd.ExposedUpdateContextWithCredentials(ctx, common.S3GatewayRootUser, common.S3GatewayRootPassword, common.S3GatewayDefaultRegion)
+		} else {
+			// We may now have an updated secret key inferred from accessToken.
+			secret := common.S3GatewayRootPassword
+			if sp := claims.GetSecretPair(); sp != "" {
+				secret = sp
+			}
+			ctx = cmd.ExposedUpdateContextWithCredentials(ctx, accessToken, secret, common.S3GatewayDefaultRegion)
 		}
-		ctx = cmd.ExposedUpdateContextWithCredentials(ctx, accessToken, secret, common.S3GatewayDefaultRegion)
 		r = r.WithContext(ctx)
 		if errCode := cmd.ExposedValidateRequestSignature(r); errCode != cmd.ErrNone {
 			cmd.ExposedWriteErrorResponse(ctx, w, errCode, r.URL)
@@ -129,17 +144,23 @@ func (a *pydioAuthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (a *pydioAuthHandler) extractToken(ctx context.Context, r *http.Request) (token string, er error) {
+func (a *pydioAuthHandler) extractToken(ctx context.Context, r *http.Request) (token string, fromPydioJWT bool, er error) {
 
 	rq := r.URL.Query()
 
 	if bearer := r.Header.Get("X-Pydio-Bearer"); bearer != "" {
 
-		return "", errors.New("X-Pydio-Bearer authentication method is deprecated")
+		return "", false, errors.New("X-Pydio-Bearer authentication method is deprecated")
 
 	} else if jwt := rq.Get("pydio_jwt"); len(jwt) > 0 {
 
-		return "", errors.New("pydio_jwt authentication method is deprecated")
+		rq.Del("pydio_jwt")
+		if r.Method == http.MethodGet && !strings.HasPrefix(strings.TrimSpace(rq.Get("response-content-disposition")), "attachment") {
+			rq.Set("response-content-disposition", "attachment")
+		}
+		r.URL.RawQuery = rq.Encode()
+		_ = r.ParseForm()
+		return jwt, true, nil
 
 	}
 
@@ -147,8 +168,55 @@ func (a *pydioAuthHandler) extractToken(ctx context.Context, r *http.Request) (t
 	token, s3Err = cmd.ExposedExtractKeyFromSignature(r)
 
 	if s3Err != cmd.ErrNone {
-		return "", errors.New(s3Err.String())
+		return "", false, errors.New(s3Err.String())
 	}
 
 	return
+}
+
+func (a *pydioAuthHandler) resignPydioJWTPresignedRequest(r *http.Request) (*http.Request, bool) {
+	if !isPresignedV4(r.URL.Query()) {
+		return r, false
+	}
+	if _, errCode := cmd.ExposedExtractKeyFromSignature(r); errCode != cmd.ErrNone {
+		return r, false
+	}
+	return presignWithGatewayCredentials(r), true
+}
+
+func isPresignedV4(query url.Values) bool {
+	return query.Get("X-Amz-Credential") != "" && query.Get("X-Amz-Signature") != "" && query.Get("X-Amz-Date") != ""
+}
+
+func presignWithGatewayCredentials(r *http.Request) *http.Request {
+	origURL := r.URL
+	origHeader := r.Header
+	newURL := *origURL
+	newQuery := url.Values{}
+	expire := int64(900)
+
+	for k, vv := range origURL.Query() {
+		if k == "X-Amz-Expires" {
+			if ex, e := strconv.ParseInt(strings.Join(vv, ""), 10, 64); e == nil {
+				expire = ex
+			}
+		}
+		if strings.HasPrefix(k, "X-Amz-") || k == "pydio_jwt" {
+			continue
+		}
+		for _, v := range vv {
+			newQuery.Add(k, v)
+		}
+	}
+	newURL.RawQuery = newQuery.Encode()
+
+	newReq := *r
+	newReq.URL = &newURL
+	newReq.RequestURI = newURL.RequestURI()
+	_ = newReq.ParseForm()
+	signed := signer.PreSignV4(newReq, common.S3GatewayRootUser, common.S3GatewayRootPassword, "", common.S3GatewayDefaultRegion, expire)
+	if len(origHeader) > 0 {
+		signed.Header = origHeader.Clone()
+	}
+	return signed
 }
