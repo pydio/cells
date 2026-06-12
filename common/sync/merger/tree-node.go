dd779d44d06433dd9a8a52754f67b1e8c6bcefe3
Osama Salama <osama.salama@wire.com>
2026-06-12 16:36:06 +0200

    fix(sync): remove return statement

--- common/sync/merger/tree-node.go
+++ common/sync/merger/tree-node.go
@@ -127,8 +127,6 @@ func TreeNodeFromSource(ctx context.Context, source model.PathSyncSource, root s
 					if statusChan != nil {
 						statusChan <- model.NewProcessingStatus(fmt.Sprintf("Could not compute hash for %s", p)).SetEndpoint(uri).SetNode(node).SetError(e)
 					}
-					// Test Trial to skip adding nodes which failed Checksum Calc.
-					return
 				}
 				childNode := NewTreeNode(node)
 				parent.AddChild(childNode)
